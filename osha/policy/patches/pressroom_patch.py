c7c3ecd76a35ab429a51e5dd8ac1569bef42e306
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-03-27 12:55:06 +0000

    change languageIndependent settings in PressClippings

---
+++ osha/policy/patches/pressroom_patch.py
@@ -0,0 +1,4 @@
+from Products.PressRoom.content.PressRelease import schema as PressReleaseSchema
+
+PressReleaseSchema['subhead'].languageIndependent = False
+PressReleaseSchema['releaseDate'].languageIndependent = True
