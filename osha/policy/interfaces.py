7afc0cff81282bb2a0985f21999fc7b95773e6ca
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-04-15 09:30:17 +0000

    added new marker interface for seps

--- osha/policy/interfaces.py
+++ osha/policy/interfaces.py
@@ -2,9 +2,12 @@ from zope.interface import Interface, alsoProvides
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
 
-class ISubsite(Interface):
-    """ A subsite which is identified by a keyword, can have its own skin, ...
+class ISubsite(INavigationRoot):
+    """ A subsite which can have its own skin and navigation
     """
     
-#alsoProvides(ISubsite, INavigationRoot)
+class ISingleEntryPoint(Interface):
+    """ A SEP which is identified by a keyword, can have its own skin, ...
+    """    
+
     
