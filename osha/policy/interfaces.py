e5212bc8ad96f438d13c6b4786d8ef11e5928f94
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-04-16 17:54:05 +0000

    removed stale interface, excluded some more items from navi

--- osha/policy/interfaces.py
+++ osha/policy/interfaces.py
@@ -1,10 +1,4 @@
 from zope.interface import Interface, alsoProvides
-from plone.app.layout.navigation.interfaces import INavigationRoot
-
-
-class ISubsite(INavigationRoot):
-    """ A subsite which can have its own skin and navigation
-    """
     
 class ISingleEntryPoint(Interface):
     """ A SEP which is identified by a keyword, can have its own skin, ...
