a04fe3f75b63ad87f7b05a3537434d2c29914454
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-01 13:38:03 +0000

    added LanguagSetter

--- osha/policy/Extensions/helperFux.py
+++ osha/policy/Extensions/helperFux.py
@@ -3,22 +3,32 @@ from plone.portlets.constants import CONTEXT_CATEGORY, GROUP_CATEGORY, CONTENT_T
 from Products.OSHATranslations import OSHAMessageFactory as _
 
 def helper(self):
-  print "\nhelper"
-
-  path = '/'.join(self.getPhysicalPath())
-
-  portal = self.portal_url.getPortalObject()
-  portal_path = self.portal_url.getPortalPath()
-  langs = portal.portal_languages.getSupportedLanguages()
-
-  right = assignment_mapping_from_key(self, 'plone.rightcolumn', CONTEXT_CATEGORY, path)
-  print right.keys()
-  from plone.portlet.collection import collection as collection_portlet
-
-  key = 'organisation'
-  topic_path = "%s/directory/%s/Publication" %(portal_path, key)
-  right[u'sep_publications'] = collection_portlet.Assignment(header=_(u"legend_publications"), 
+    print "\nhelper"
+    
+    path = '/'.join(self.getPhysicalPath())
+    
+    portal = self.portal_url.getPortalObject()
+    portal_path = self.portal_url.getPortalPath()
+    langs = portal.portal_languages.getSupportedLanguages()
+    
+    right = assignment_mapping_from_key(self, 'plone.rightcolumn', CONTEXT_CATEGORY, path)
+    print right.keys()
+    from plone.portlet.collection import collection as collection_portlet
+    
+    key = 'organisation'
+    topic_path = "%s/directory/%s/Publication" %(portal_path, key)
+    right[u'sep_publications'] = collection_portlet.Assignment(header=_(u"legend_publications"), 
                                    target_collection=topic_path, 
                                    limit=5)
-  
-  return "ok"
+    
+    return "ok"
+
+
+def LanguageSetter(self, lang=''):
+    print "\n LanguageSetter"
+    
+    objs = self.objectValues()
+    for obj in objs:
+        obj._md['language'] = lang
+
+    return "ok"
