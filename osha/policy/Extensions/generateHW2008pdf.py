3d663d7d86bf676a563d2f8786a95226e4e60853
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-05 19:23:22 +0000

    ported to py2.4

--- osha/policy/Extensions/generateHW2008pdf.py
+++ osha/policy/Extensions/generateHW2008pdf.py
@@ -66,50 +66,27 @@ def generatePDF(self, company="European Agency for Safety and Health at Work", l
 
 #    # print campaign-slogan
     msg_id = 'campaign_slogan_'+year
-    u_campaign_slogan = ptt.translate(ptt_domain, msg_id, target_language=language)
-#    campaign_slogan = u_campaign_slogan.encode('utf-8')
-#    print campaign_slogan, type(campaign_slogan)
-#    x =  4.5 * cm
-#    y = 18.8 * cm
-#    if my_canvas.stringWidth(campaign_slogan, 'ArialBoldItalic', 30) > 300:
-#        y = 18.5 * cm
-#    my_canvas.setFont('ArialBoldItalic', 30)
-#    my_canvas.setFillColorRGB(0.5, 0.5, 0.5)
-#    my_canvas.drawString(x+(0.07*cm), y-(0.05*cm), campaign_slogan)
-#    my_canvas.setFillColorRGB((252.0/255), (176.0/255), (61.0/255))
-#    my_canvas.drawString(x, y, campaign_slogan)
-#    print " +- set campaign slogan"#, campaign_slogan
-#
-    # prepare the replacement-mapping for the coming u_translations
-    mapping = {'campaign_slogan':u_campaign_slogan, 'year':year}
-#    print " +--Mapping:", mapping
-#
-#
-#    # print EW-slogan
-#    # if the Campaign-Slogan is taller than the half of the page-width
-#    # then print the EW-Slogan above the Campaign slogan instead of the same height
-#    ew_slogan = ptt.utranslate(ptt_domain, 'ew_slogan', mapping=mapping, target_language=language)
-#    ew_slogan = ew_slogan.encode('utf-8')
-#    x = 28.55* cm
-#    y = 19.2 * cm
-#    if my_canvas.stringWidth(campaign_slogan, 'ArialBoldItalic', 30) > 300:
-#        y = 19.6 * cm
-#    my_canvas.setFont('ArialNarrowBold', 14)
-#    my_canvas.setFillColorRGB(0, 0, 0)
-#    my_canvas.drawRightString(x, y, ew_slogan)
-#    print " +- set EW slogan"
-#
-    # print headline
-    certificate_title = ptt.utranslate(ptt_domain, 'certificate_title_'+year, target_language=language)
-    certificate_title = certificate_title.encode('utf-8')
+    u_campaign_slogan = ptt.translate(domain=ptt_domain, msgid=msg_id, target_language=language, context=self)
+
+    
+    certificate_title = ptt.translate(domain=ptt_domain, 
+                                      msgid='certificate_title_'+year, 
+                                      target_language=language, 
+                                      context=self)
+    #certificate_title = certificate_title.encode('utf-8')
     x = 14.85 * cm
     y = 16.6 * cm
     my_canvas.setFont('Arial', 30)
     my_canvas.drawCentredString(x, y, certificate_title.upper())
     print " +- set Headline"#, certificate_title
-
+    mapping = {'campaign_slogan':u_campaign_slogan, 'year':year}
+        
     # print first subline
-    certificate_for = ptt.utranslate(ptt_domain, 'certificate_for_'+year, mapping=mapping, target_language=language)
+    certificate_for = ptt.translate(domain=ptt_domain, 
+                                    msgid='certificate_for_'+year, 
+                                    mapping=mapping, 
+                                    target_language=language,
+                                    context=self)
     certificate_for = certificate_for.encode('utf-8')
     x = 14.85 * cm
     y = 15.0 * cm
@@ -135,7 +112,11 @@ def generatePDF(self, company="European Agency for Safety and Health at Work", l
         )
 
     lines = []
-    contribution_headline = ptt.utranslate(ptt_domain, 'contribution_headline_'+year, mapping=mapping, target_language=language)
+    contribution_headline = ptt.translate(domain=ptt_domain, 
+                                          msgid='contribution_headline_'+year, 
+                                          mapping=mapping, 
+                                          target_language=language,
+                                          context=self)
     contribution_headline = contribution_headline.encode('utf-8')
     lines.append( Paragraph(contribution_headline, style) )
     cFrame = Frame(2*cm, 9.0*cm, 25.7*cm, 3*cm)
@@ -144,7 +125,11 @@ def generatePDF(self, company="European Agency for Safety and Health at Work", l
     # Director's signature
     director_name = "Jukka Takala,"
     director_name = director_name.encode('utf-8')
-    director_indentifiyier = ptt.utranslate(ptt_domain, 'director', mapping=mapping, target_language=language)
+    director_indentifiyier = ptt.translate(domain=ptt_domain, 
+                                           msgid='director', 
+                                           mapping=mapping, 
+                                           target_language=language,
+                                           context=self)
     director_indentifiyier = director_indentifiyier.encode('utf-8')
     
     style = ParagraphStyle(
@@ -161,69 +146,6 @@ def generatePDF(self, company="European Agency for Safety and Health at Work", l
     dFrame = Frame(3.5*cm, 3.7*cm, 5*cm, 3*cm)
     dFrame.addFromList(lines, my_canvas)
     
-    
-#    contribution_subline = ptt.utranslate(ptt_domain, 'contribution_subline', mapping=mapping, target_language=language)
-#    contribution_subline = contribution_subline.encode('utf-8')
-#    if contribution_subline.find('${campaign_slogan}') > -1:
-#        contribution_subline = contribution_subline.replace('${campaign_slogan}', campaign_slogan)
-#    lines.append( Paragraph(contribution_headline, style) )
-#    lines.append( Paragraph(contribution_subline, style) )
-#
-#    cFrame = Frame(2*cm, 11*cm, 25.7*cm, 2.5*cm)
-#    cFrame.addFromList(lines, my_canvas)
-#    print " +- set contribution headline"#, contribution_headline, contribution_subline
-#
-#    # print actions of subscriber
-#    style = ParagraphStyle(
-#            name='CustomActivities',
-#            fontName='Arial',
-#            fontSize=12,
-#            bulletFontName='Symbol',
-#            bulletFontSize=12,
-#            bulletIndent=5,
-#            leftIndent = 20,
-#            leading=15
-#        )
-#    x_left = 2.1 * cm
-#    y_left = 1.4 * cm
-#    aw_left = 12.3 * cm
-#    ah_left = 10 * cm
-#
-#    x_right = 15.5 * cm
-#    y_right = y_left
-#    aw_right = aw_left
-#    ah_right = ah_left
-#
-#    act_keys= checkboxes.keys()
-#    act_keys.sort()
-#    frame_left = Frame(x_left, y_left, aw_left, ah_left, 0 ,0 ,0 ,0)
-#    frame_right = Frame(x_right, y_right, aw_right, ah_right, 0, 0, 0, 0)
-#    act_left = []
-#    act_right = []
-#    idx = 0
-#    print "act_keys:", act_keys
-#    for k in act_keys:
-#        print "key:",  k
-#        act = "<bullet/>%s" % (checkboxes[k])
-#        P = Paragraph(act, style, '\267')
-#        if idx % 2:
-#            act_left.append(P)
-#        else:
-#            act_right.append(P)
-#        idx = idx + 1
-#
-#    frame_left.addFromList(act_left, my_canvas)
-#    frame_right.addFromList(act_right, my_canvas)
-#    print " +- set subscribed actions"
-
-#    # print name of subscriber
-#    x = 14.85 * cm
-#    y = 3.5 * cm
-#    font_size_name = 14
-#    name = "%s %s" %(firstname, lastname)
-#    my_canvas.setFont('Arial', font_size_name)
-#    my_canvas.drawCentredString(x, y, name)
-#    print " +- set subscriber name"
     
     # ***** FALLBACK-SOLUTION UNTIL PDFTK IS AVAILABLE *******
     if usePDFTK == 0:
