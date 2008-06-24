cdffbadd2acbdcebda29fc80abf4cbd14ab579f9
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-06-24 13:02:29 +0000

    ifix ftypo

--- osha/policy/patches/email_as_login_patch.py
+++ osha/policy/patches/email_as_login_patch.py
@@ -1,6 +1,7 @@
 from Products.CMFPlone.MembershipTool import MembershipTool
 from Products.CMFPlone.RegistrationTool import RegistrationTool
 from simplon.plone.ldap.ploneldap.util import getLDAPPlugin
+from Products.CMFCore.utils import getToolByName
 
 def testCurrentPassword(self, password):
     """ test to see if password is current """
@@ -16,12 +17,10 @@ def testCurrentPassword(self, password):
     
 MembershipTool.testCurrentPassword = testCurrentPassword
 
-def _get_userid_by_email(self, email):
-
     
-security.declarePublic('mailPassword')
 def email_mailPassword(self, email, REQUEST):
     """ Wrapper around mailPassword """
+    import pdb;pdb.set_trace()
     registration = getToolByName(self, 'portal_registration')
 
     forgotten_userid = ''
@@ -29,7 +28,11 @@ def email_mailPassword(self, email, REQUEST):
     result = luf._lookupuserbyattr('mail', email)
     if len(result)>3:
         attrs = result[2]
-        forgotten_userid = attrs.get('uid')
+        try:
+            forgotten_userid = attrs.get('uid')[0]
+        except:
+            pass
+        
     
     registration.mailPassword(forgotten_userid, REQUEST)
 
