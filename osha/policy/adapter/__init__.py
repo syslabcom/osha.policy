d551993375bcfbda3706a681325d2bef03e07bbb
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-08-19 12:44:21 +0000

    removed retriever for richdocument, this is no longer necessary

--- osha/policy/adapter/__init__.py
+++ osha/policy/adapter/__init__.py
@@ -3,9 +3,4 @@ logger = logging.getLogger("osha.policy")
 
 import schemaextender
 
-# import lms retrievers if we have Linkchecker installed
-try:
-    import lmsretrievers
-except:
-    logger.info('Not importing LMS Retrievers. CMFLinkchecker not present?')
     
