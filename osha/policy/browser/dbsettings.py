4687e06d0b406f072cca74352655e9dc90c0538d
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-08-26 07:07:36 +0000

    added controlpanel to edit database connectivity

---
+++ osha/policy/browser/dbsettings.py
@@ -0,0 +1,21 @@
+from zope.component import getUtility 
+from zope.formlib import form 
+from plone.app.controlpanel.form import ControlPanelForm 
+from collective.lead.interfaces import IDatabase 
+from osha.policy.interfaces import IDatabaseSettings 
+from osha.theme import OSHAMessageFactory as _
+
+def osha_database_settings(context): 
+    return getUtility(IDatabaseSettings) 
+
+class OSHADatabaseControlPanel(ControlPanelForm): 
+
+    form_fields = form.FormFields(IDatabaseSettings) 
+    form_name = _(u"OSHA Database settings") 
+    label = _(u"OSHA Database settings") 
+    description = _(u"Please enter the appropriate connection settings" 
+                      "for the database") 
+
+    def _on_save(self, data): 
+        db = getUtility(IDatabase, name='osha.database') 
+        db.invalidate() 
--- osha/policy/interfaces.py
+++ osha/policy/interfaces.py
@@ -1,7 +1,33 @@
 from zope.interface import Interface, alsoProvides
-    
+from zope import schema
+from osha.theme import OSHAMessageFactory as _
+
 class ISingleEntryPoint(Interface):
     """ A SEP which is identified by a keyword, can have its own skin, ...
     """    
 
+class IDatabaseSettings(Interface):
+    """ Settings to access the RDBMS """
+    drivername = schema.ASCIILine(title=_(u"Driver name"), 
+                   description=_(u"The database driver name"), 
+                   default='mysql', 
+                   required=True) 
+    hostname = schema.ASCIILine(title=_(u"Host name"), 
+                   description=_(u"The database host name"), 
+                   default='localhost', 
+                   required=True) 
+                                
+    port = schema.Int(title=_(u"Port number"), 
+                   description=_(u"The database port number. " 
+                                   "Leave blank to use the default."), 
+                   required=False) 
+    username = schema.ASCIILine(title=_(u"User name"), 
+                   description=_(u"The database user name"), 
+                   required=True) 
+    password = schema.Password(title=_(u"Password"), 
+                   description=_(u"The database password"), 
+                   required=False) 
+    database = schema.ASCIILine(title=_(u"Database name"), 
+                   description=_(u"The name of the database on this server"), 
+                   required=True) 
     
