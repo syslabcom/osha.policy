a4889c47f7a519bf7905b71817a883864520cda1
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-09-11 13:41:12 +0000

    added patch to call index_html if requested instead of download

--- osha/policy/patches/blob_response_header_patch.py
+++ osha/policy/patches/blob_response_header_patch.py
@@ -3,11 +3,12 @@
 # remove if fixed by plone.app.blob
 
 from plone.app.blob.field import BlobField
+from plone.app.blob.content import ATBlob
 from plone.i18n.normalizer.interfaces import IUserPreferredFileNameNormalizer
 from Products.Archetypes.utils import contentDispositionHeader
+from webdav.common import rfc1123_date
 
-
-def index_html(self, instance, REQUEST=None, RESPONSE=None, disposition='inline'):
+def field_index_html(self, instance, REQUEST=None, RESPONSE=None, disposition='inline'):
     """ make it directly viewable when entering the objects URL """
     if REQUEST is None:
         REQUEST = instance.REQUEST
@@ -22,8 +23,17 @@ def index_html(self, instance, REQUEST=None, RESPONSE=None, disposition='inline'
             filename=filename)
         RESPONSE.setHeader("Content-disposition", header_value)
     blob = self.get(instance, raw=True)     # TODO: why 'raw'?
+    RESPONSE.setHeader('Last-Modified', rfc1123_date(instance._p_mtime))
     RESPONSE.setHeader('Content-Type', instance.content_type)
     RESPONSE.setHeader("Content-Length", blob.get_size())
     return blob.getIterator()
         
-BlobField.index_html = index_html        
+        
+        
+def file_index_html(self, REQUEST, RESPONSE):
+    """ download the file inline """
+    field = self.getPrimaryField()
+    return field.index_html(self, REQUEST, RESPONSE)
+                
+BlobField.index_html = field_index_html        
+ATBlob.index_html = file_index_html
