d589bfd6a49f017f69aceb44cbe4152db626d425
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-11-24 01:42:27 +0000

    thesaurus portlet adapter now shows parents

--- osha/policy/adapter/clicksearch_support.py
+++ osha/policy/adapter/clicksearch_support.py
@@ -21,6 +21,8 @@ class MultilingualThesaurusView(SimpleListView):
         vocab = getattr(vtool, 'MultilingualThesaurus')
         self.manager = vocab._getManager()
 
+        self.reverse_dict = self.create_reverse_term_dict()
+
         if index in self.request.keys():
             return self.template_selected()
 
@@ -28,17 +30,53 @@ class MultilingualThesaurusView(SimpleListView):
     
     def selected_value_caption(self):
         return self.manager.getTermCaptionById(self.request.get(self.index))
+    
+    def create_reverse_term_dict(self):
+        """ create a dict which allows to resolve the parents"""
+        vdict = self.manager.getVocabularyDict()
+        reverse_dict = {}
+        
             
+        def register_parent(parent, dict):
+            for k,v in dict.items():
+                new_parent = k
+                reverse_dict[k] = parent
+                if v[1] is None:
+                    continue
+                register_parent(new_parent, v[1])
+                
+        register_parent(None, vdict)
+        
+        return reverse_dict
+        
+    def get_parents(self):
+        """ returns a list of captions for all parents of the given term """
+        parentlist = []
+        X = self.selected_value()
+        query = current_query(self.request)
+        while True:
+            X = self.reverse_dict.get(X)
+            if X is None:
+                break
+            caption = self.manager.getTermCaptionById(X)
+            query['multilingual_thesaurus'] = X
+            new_qs = make_qs(query)
+            parentlist.append(dict(id=X, title=caption, link="clicksearch?%s" % new_qs))
+        parentlist.reverse()
+        return parentlist
+        
     def children(self):
+        children = []
         term = self.selected_value()
         term_node = self.manager.getTermById(term)
         terms = self.manager.getTerms(term_node)
-        children = []
+        if terms is None:
+            return children
         query = current_query(self.request)
         for key in terms.keys():
             query['multilingual_thesaurus'] = key
             new_qs = make_qs(query)
             children.append(dict(id=key, title=terms[key][0], link="clicksearch?%s" % new_qs))
         return children
-        
+
     
