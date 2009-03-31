ae7e08edbb102305fdcdde4fb047480ff3a6b459
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-03-31 16:06:29 +0000

    Make releaseContacts languageIndependent

--- osha/policy/patches/pressroom_patch.py
+++ osha/policy/patches/pressroom_patch.py
@@ -2,3 +2,4 @@ from Products.PressRoom.content.PressRelease import schema as PressReleaseSchema
 
 PressReleaseSchema['subhead'].languageIndependent = False
 PressReleaseSchema['releaseDate'].languageIndependent = True
+PressReleaseSchema['releaseContacts'].languageIndependent = True
