4305d6e686d4fdfb7415290f189f504ec910e988
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-04-06 09:57:48 +0000

    Fix regression

--- osha/policy/Extensions/cleanWordPastedText.py
+++ osha/policy/Extensions/cleanWordPastedText.py
@@ -7,14 +7,20 @@ from Products.CMFCore.utils import getToolByName
 
 def run(self):
     """ """
-    for o in queryObjs(self):
-        import pdb; pdb.set_trace()
+    num_fs = 0
+    num_o = 0
+    portal_type = self.REQUEST.get('portal_type', 'OSH_Link')
+    for o in queryObjs(self, portal_type):
         fs = getRichTextFields(o)
-        for f in fs:
-            text = santize(self, f.get(), documentCleaner)
+        num_fs += len(fs)
+        if len(fs):
+            num_o += 1
+            for f in fs:
+                text = santize(self, f.get(), documentCleaner)
+                f.set(text)
+    return 'Cleaned up %d fields in %d %s objects' % (num_fs, num_o, portal_type)
 
-def queryObjs(self):
-    portal_type = self.REQUEST.get('portal_type', 'OSH⊇Link')
+def queryObjs(self, portal_type):
     catalog = getToolByName(self, 'portal_catalog')
     return [o.getObject() for o in catalog(portal_type=portal_type)]
 
