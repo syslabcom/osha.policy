f400301537cf7cd0067b10906138afc1993f3a4b
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-04-15 11:49:45 +0000

    added

---
+++ osha/policy/Extensions/LCRetrieveByPath.py
@@ -0,0 +1,39 @@
+import transaction
+from zLOG import LOG, INFO
+
+def retrieve(self):
+  pc = self.portal_catalog
+  link_checker = self.portal_linkchecker
+  ST = []
+  langs = self.portal_languages.getSupportedLanguages()
+  #langs = ['sl', 'sk', 'sv']
+  path = "/osha/portal/fop/france"
+  #lpath = "/osha/portal/fop/france"
+  LOG("LCRetrieveByType", INFO, "Starting retrieval")
+  langs=["en"]
+  for lang in langs:
+    LOG("LCRetrieveByType", INFO, "> Language %s"%lang)
+    results = pc(Language='all',  path=path)
+    #results = pc(Language='all',  path=lpath%lang)
+    #results = pc(Language='all', portal_type=portal_type, path=lpath%lang)
+    LOG("LCRetrieveByType", INFO, "> %s results found"%len(results))
+    cnt = 0
+    for result in results:
+      try:
+        object = result.getObject()
+      except:
+        continue
+      if not object: continue
+      link_checker.retrieving.retrieveObject(object)
+      LOG("LCRetrieveByType", INFO, "> Retrieved %s"%result.getPath())
+      ST.append("retrieved %s" % result.getPath())
+      cnt += 1
+      if cnt%10==0:
+        transaction.commit()
+        LOG("LCRetrieveByType", INFO, "Committing %s of %s" %(cnt, len(results)))
+        #transaction.savepoint()
+
+  LOG("LCRetrieveByType", INFO, "Fertig")
+  return "\n".join(ST)
+
+
