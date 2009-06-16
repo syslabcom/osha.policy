b27918659b8dca1b71a8d55818dda9b7dfb7bf8e
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-16 14:20:13 +0000

    Fixed all non running tests

--- osha/policy/tests/base.py
+++ osha/policy/tests/base.py
@@ -5,84 +5,86 @@ from Testing import ZopeTestCase as ztc
 
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
+from Products.PloneTestCase import layer
 
-# These are traditional products (in the Products namespace). They'd normally
-# be loaded automatically, but in tests we have to load them explicitly. This
-# should happen at module level to make sure they are available early enough.
+SiteLayer = layer.PloneSite
 
-ztc.installProduct('ATVocabularyManager')
-ztc.installProduct('CallForContractors')
-ztc.installProduct('CaseStudy')
-ztc.installProduct('CMFSin')
-ztc.installProduct('CMFLinkChecker')
-ztc.installProduct('Clouseau')
-ztc.installProduct('FCKeditor')
-ztc.installProduct('LinguaPlone')
-ztc.installProduct('OSHContentLink')
-ztc.installProduct('Publications')
-ztc.installProduct('PublicJobVacancy')
-ztc.installProduct('RALink')
-ztc.installProduct('SEPStructure')
-ztc.installProduct('VocabularyPickerWidget')
-ztc.installProduct('CMFPlacefulWorkflow')
-ztc.installProduct('plone.browserlayer')
-ztc.installProduct('plone.app.iterate')
-ztc.installProduct('RichDocument')
-ztc.installProduct('Marshall')
-ztc.installProduct('collective.portlet.feedmixer')
-ztc.installProduct('collective.portlet.tal')
-ztc.installProduct('plone.portlet.collection')
-ztc.installProduct('plone.portlet.static')
-#ztc.installProduct('PressRoom')
-ztc.installProduct('ATCountryWidget')
-ztc.installProduct('DataGridField')
-ztc.installProduct('TextIndexNG3')
-ztc.installProduct('UserAndGroupSelectionWidget')
-ztc.installProduct('simplon.plone.ldap')
-ztc.installProduct('ProxyIndex')
-ztc.installProduct('ZCatalog')
-ztc.installProduct('Relations')
 
-@onsetup
-def setup_osha_policy():
-    """Set up the additional products required for the OSHA site policy.
-    
-    The @onsetup decorator causes the execution of this body to be deferred
-    until the setup of the Plone site testing layer.
-    """
-    
-    # Load the ZCML configuration for the osha.policy package.
-    # This includes the other products below as well.
-    
-    fiveconfigure.debug_mode = True
-    import osha.policy
-    import slc.seminarportal
-    zcml.load_config('configure.zcml', osha.policy)
-    zcml.load_config('configure.zcml', slc.seminarportal)
-    fiveconfigure.debug_mode = False
-    
-    # We need to tell the testing framework that these products
-    # should be available. This can't happen until after we have loaded
-    # the ZCML.
-       
-    ztc.installPackage('osha.theme')
-    ztc.installPackage('osha.policy')
-    ztc.installPackage('slc.seminarportal')
-    ztc.installPackage('osha.legislation')
-    
-# The order here is important: We first call the (deferred) function which
-# installs the products we need for the Optilux package. Then, we let 
-# PloneTestCase set up this product on installation.
+class OSHAPolicyLayer(SiteLayer):
 
-setup_osha_policy()
-ptc.setupPloneSite(products=['osha.policy', 'CMFLinkChecker'])
+    @classmethod
+    def getPortal(cls):
+        import pdb;pdb.set_trace()
+        app = ZopeTestCase.app()
+        portal = app._getOb(portal_name)
+        _placefulSetUp(portal)
+        return portal
+
+    @classmethod
+    def setUp(cls):
+        ptc.setupPloneSite(products=['osha.policy', 'CMFLinkChecker'])
+
+        ztc.installProduct('ATVocabularyManager')
+        ztc.installProduct('CallForContractors')
+        ztc.installProduct('CaseStudy')
+        ztc.installProduct('CMFSin')
+        ztc.installProduct('CMFLinkChecker')
+        ztc.installProduct('Clouseau')
+        ztc.installProduct('FCKeditor')
+        ztc.installProduct('LinguaPlone')
+        ztc.installProduct('OSHContentLink')
+        ztc.installProduct('Publications')
+        ztc.installProduct('PublicJobVacancy')
+        ztc.installProduct('RALink')
+        ztc.installProduct('SEPStructure')
+        ztc.installProduct('VocabularyPickerWidget')
+        ztc.installProduct('CMFPlacefulWorkflow')
+        ztc.installProduct('plone.browserlayer')
+        ztc.installProduct('plone.app.iterate')
+        ztc.installProduct('RichDocument')
+        ztc.installProduct('Marshall')
+        ztc.installProduct('collective.portlet.feedmixer')
+        ztc.installProduct('collective.portlet.tal')
+        ztc.installProduct('plone.portlet.collection')
+        ztc.installProduct('plone.portlet.static')
+        #ztc.installProduct('PressRoom')
+        ztc.installProduct('ATCountryWidget')
+        ztc.installProduct('DataGridField')
+        ztc.installProduct('TextIndexNG3')
+        ztc.installProduct('UserAndGroupSelectionWidget')
+        ztc.installProduct('simplon.plone.ldap')
+        ztc.installProduct('ProxyIndex')
+        ztc.installProduct('ZCatalog')
+        ztc.installProduct('Relations')
+
+        fiveconfigure.debug_mode = True
+        import osha.policy
+        import slc.seminarportal
+        zcml.load_config('configure.zcml', osha.policy)
+        zcml.load_config('configure.zcml', slc.seminarportal)
+        fiveconfigure.debug_mode = False
+
+        # We need to tell the testing framework that these products
+        # should be available. This can't happen until after we have loaded
+        # the ZCML.
+
+        ztc.installPackage('osha.theme')
+        ztc.installPackage('osha.policy')
+        ztc.installPackage('slc.seminarportal')
+        ztc.installPackage('osha.legislation')
+
+        SiteLayer.setUp()
+
+
 
 class OSHAPolicyTestCase(ptc.PloneTestCase):
     """We use this base class for all the tests in this package. If necessary,
     we can put common utility or setup code in here.
     """
+    layer = OSHAPolicyLayer
 
 class OSHAPolicyFunctionalTestCase(ptc.FunctionalTestCase):
     """We use this base class for all the tests in this package. If necessary,
     we can put common utility or setup code in here.
     """
+    layer = OSHAPolicyLayer
