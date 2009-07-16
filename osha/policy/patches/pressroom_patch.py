f1a6fc0bb258d5f1a23e7ba2175fd050b82e308f
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-07-16 11:35:14 +0000

    patching Press Release: Date() now returns storydate (==release date)

--- osha/policy/patches/pressroom_patch.py
+++ osha/policy/patches/pressroom_patch.py
@@ -1,5 +1,12 @@
 from Products.PressRoom.content.PressRelease import schema as PressReleaseSchema
+from Products.PressRoom.content.PressRelease import PressRelease
 
 PressReleaseSchema['subhead'].languageIndependent = False
 PressReleaseSchema['releaseDate'].languageIndependent = True
 PressReleaseSchema['releaseContacts'].languageIndependent = True
+
+def Date(self):
+    """Avoid the need for a getStoryDate metadatum in the catalog"""
+    return self.getStorydate()
+
+PressRelease.Date = Date
