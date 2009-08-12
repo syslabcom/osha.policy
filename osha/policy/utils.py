f5ab9bb8775d20352ac4984a8831dc40762167ca
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-08-12 19:36:35 +0000

    reverting utils and handlers. reason: ConfigurationError: ('Invalid value for', 'for', "ImportError: Couldn't import slc.autotranslate.interfaces, No module named interfaces in slc.autotranslate.interfaces.IAutoTranslatedFlashUploadEvent")

--- osha/policy/utils.py
+++ osha/policy/utils.py
@@ -1,5 +1,5 @@
-from pyPdf.pdf import ContentStream
-from pyPdf.generic import *
+
+
 
 def logit(*kwargs):
     " log something from the web "
@@ -10,37 +10,3 @@ def logit(*kwargs):
         print mesg
     except:
         print [kwargs]
-
-def extractPDFText(self):
-    text = u""
-    content = self["/Contents"].getObject()
-    if not isinstance(content, ContentStream):
-        content = ContentStream(content, self.pdf)
-    # Note: we check all strings are TextStringObjects.  ByteStringObjects
-    # are strings where the byte->string encoding was unknown, so adding
-    # them to the text here would be gibberish.
-    for operands,operator in content.operations:
-        if operator == "Tj":
-            _text = operands[0]
-            if isinstance(_text, TextStringObject):
-                text += _text
-        elif operator == "T*":
-            text += "\n"
-        elif operator == "'":
-            text += "\n"
-            _text = operands[0]
-            if isinstance(_text, TextStringObject):
-                text += operands[0]
-        elif operator == '"':
-            _text = operands[2]
-            if isinstance(_text, TextStringObject):
-                text += "\n"
-                text += _text
-        elif operator == "TJ":
-            for i in operands[0]:
-                if isinstance(i, TextStringObject):
-                    text += i
-        elif operator == "k":
-            text += "\n" 
-    return text
-
