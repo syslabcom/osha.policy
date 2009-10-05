ff14a1f3bba426c88a46146205ec6554b18e5ea6
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-05 12:14:56 +0000

    new

---
+++ osha/policy/Extensions/LCRetrieveByDate.py
@@ -0,0 +1,49 @@
+import transaction
+import zLOG
+from DateTime import DateTime
+
+def LCRetrieveByDate(self, skiplist=[]):
+    """Retrieves the links from all objects in the site by Date."""
+    sincedate = DateTime()-6*30
+    lc = self.portal_linkchecker
+    server = lc.database._getWebServiceConnection()
+    if server is None:
+        raise RuntimeError, "The site could not be crawled because no " \
+                            "connection to the lms could be established."
+    server.setClientNotifications(False)
+
+#    # Not actually necessary on every crawl, but it doesn't seem to work
+#    # in the installer, so this seems the next logical place to do it.
+#    self.portal_catalog.reindexIndex('portal_linkchecker_uid', self.REQUEST)
+    try:
+        database = lc.database
+        # gather all objects that are of a type we can check for links
+        if 1:
+        #for type in lc.retrieving.listSupportedTypes():
+            objects = self.portal_catalog(Language='all', modified={'query':sincedate,'range':'min'})
+            os_ = len(objects)
+            i = 0
+            for ob in objects:
+                i += 1
+                zLOG.LOG("CMFLinkChecker", zLOG.BLATHER,
+                         "Site Crawl Status",
+                         "%s of %s (%s)" % (i, os_, ob.getPath()))
+                ob = ob.getObject()
+                if ob is None:
+                    # Maybe the catalog isn't up to date
+                    continue
+                #try:
+                lc.retrieving.retrieveObject(ob)
+                #except Exception,e:
+                #    zLOG.LOG('CMFLinkChecker', zLOG.INFO,
+                #      "Unable to retrieveObject for %s. Error: %s" %([ob], e))
+                if i % 500 ==0:
+                    transaction.commit()
+                    zLOG.LOG('CMFLinkChecker', zLOG.INFO,
+                        "Crawling site - commited after %d objects of type %s" %(i, type))
+            transaction.commit()
+        # Remove unused urls
+        database.cleanup()
+    finally:
+        server.setClientNotifications(True)
+
