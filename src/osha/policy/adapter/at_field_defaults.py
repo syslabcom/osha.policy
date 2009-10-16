7d336572931b605bc97e7cda26f99647a7a8d7dd
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-16 10:44:45 +0000

    enhanced the field default mechanism for 'author' of slc.publicattion to handle a special case in the Belgian FOP

--- src/osha/policy/adapter/at_field_defaults.py
+++ src/osha/policy/adapter/at_field_defaults.py
@@ -2,6 +2,8 @@
 
 from zope.interface import implements
 from Products.Archetypes.interfaces import IFieldDefaultProvider
+from slc.subsite.root import getSubsiteRoot
+from Products.CMFCore.utils import getToolByName
 
 class DefaultForPublicationAuthor(object):
     implements(IFieldDefaultProvider)
@@ -10,4 +12,9 @@ class DefaultForPublicationAuthor(object):
         self.context = context
 
     def __call__(self):
+        subsite_root = getSubsiteRoot(self.context)
+
+        if subsite_root.find("belgium") > 0:
+            return u"parliament_question_author"
+
         return u"European Agency for Safety and Health at Work"
