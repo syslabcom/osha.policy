1bc8c0b8443d8315297aa3787e6032fd655654dd
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-16 09:21:22 +0000

    specialfish: for Publications in the Belgian FOP, return other defaults for the author

--- src/osha/policy/adapter/at_field_defaults.py
+++ src/osha/policy/adapter/at_field_defaults.py
@@ -2,6 +2,8 @@
 
 from zope.interface import implements
 from Products.Archetypes.interfaces import IFieldDefaultProvider
+from slc.subsite.root import getSubsiteRoot
+from Products.CMFCore.utils import getToolByName
 
 class DefaultForPublicationAuthor(object):
     implements(IFieldDefaultProvider)
@@ -10,4 +12,14 @@ class DefaultForPublicationAuthor(object):
         self.context = context
 
     def __call__(self):
+        subsite_root = getSubsiteRoot(self.context)
+
+        if subsite_root.find("belgium") > 0:
+            lang = getToolByName(self.context, 'portal_languages').getPreferredLanguage()
+            if lang=='fr':
+                return u"Service Public Fédéral Emploi, Travail et Concertation sociale"
+            # nl is default
+            else:
+                return u"Federale Overheidsdienst Arbeid en Sociaal Overleg"
+
         return u"European Agency for Safety and Health at Work"
