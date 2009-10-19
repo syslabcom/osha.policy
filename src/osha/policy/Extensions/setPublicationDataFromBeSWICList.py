86b68e1d7caab49b0b3106350a9ef236fc5eb152
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-19 17:34:10 +0000

    this script will set title, description and publication date on the uploaded Questions in Parliament files

---
+++ src/osha/policy/Extensions/setPublicationDataFromBeSWICList.py
@@ -0,0 +1,67 @@
+from Products.CMFCore.utils import getToolByName
+import os
+
+SEPARATOR = '\t'
+
+ID = 0
+NR = 1
+DATE = 2
+TITLE_NL = 3
+TITLE_FR = 4
+DESCR_NL = 5
+DESCR_FR = 6
+PATH_NL = 7
+PATH_FR = 8
+
+def doit(self, path_to_file=""):
+    print "\n\n\nsetPublicationData from BeSWIC list"
+    if not path_to_file:
+        return "please supply path_to_file that contains the necessary data"
+    if not os.access(path_to_file, os.F_OK):
+        return "Could not open file at %s" % path_to_file
+    fh = open(path_to_file, 'r')
+    data = fh.read()
+    fh.close()
+    lines = data.split('\n')
+    print "nr of lines:", len(lines)
+    headers = lines[0].split(SEPARATOR)
+    numitems = len(headers)
+    print headers, numitems
+    i = 2
+    for line in lines[1:]:
+        if not line.strip():
+            continue
+        items = line.split(SEPARATOR)
+        if len(items)!=numitems:
+            print "ERROR wrong number of items:"
+            print i, items
+
+        
+        parts = items[NR].split(" ")
+        parts = [x for x in parts if x.strip()!='-']
+        try:
+            loc_nl, loc_fr = parts[-1].split('/')
+            rest = " ".join(parts[:-1])
+        except:
+            try:
+                #import pdb; pdb.set_trace()
+                if len(parts)>=4:
+                    if parts[-2]=='/':
+                        loc_nl = parts[-3]
+                        loc_fr = parts[-1]
+                        rest = " ".join(parts[:-3])
+                    else:
+                        raise
+                else:
+                    raise
+            except:
+                loc_nl = loc_fr = ""
+                rest = "EXTRACTING TITLE NOT POSSIBLE - PLEASE CORRECT MANUALLY"
+                print "error in line", i
+    
+        title_nl = "%s %s: %s" %(rest, loc_nl, items[TITLE_NL])
+        title_fr = "%s %s: %s" %(rest, loc_fr, items[TITLE_FR])
+        print i, title_fr
+        i+=1
+    
+    return "done - no problems"
