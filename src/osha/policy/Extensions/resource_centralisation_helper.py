00065601dcb48149f84bed1a6552c8c236f47e37
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-21 14:06:44 +0000

    Query for all languages. Fixed various bugs after running on test server

--- src/osha/policy/Extensions/resource_centralisation_helper.py
+++ src/osha/policy/Extensions/resource_centralisation_helper.py
@@ -1,7 +1,9 @@
 import logging
+import transaction
+
+from webdav.Lockable import ResourceLockedError
 
 from zope.component import queryUtility
-from zope.app.container.interfaces import IObjectAddedEvent
 
 from plone.contentrules.engine.assignments import RuleAssignment
 from plone.contentrules.engine.interfaces import IRuleStorage
@@ -19,34 +21,48 @@ log = logging.getLogger('osha_centralisation_helper')
 def run(self):
     ltool = getToolByName(self, 'portal_languages')
     langs = ltool.listSupportedLanguages()
-    f = open('var/log/osha_centralisation.log', 'w')
+    f = open('osha_centralisation.log', 'a')
+
+    # NEWS
+    # _setNewsKeywords(self, langs, f)
+    # _assignContentRules(self, 'rule-5', f, langs)
     _centraliseNews(self, langs, f)
-    _centraliseEvents(self, langs, f)
+
+    # EVENTS
+    # _setEventsKeywords(self, langs, f)
+    # _assignContentRules(self, 'rule-6', f, langs)
+    # _centraliseEvents(self, langs, f)
+
     f.close()
     return 'Done' 
 
 
-def _centraliseEvents(self, langs, f):
+def _setEventsKeywords(self, langs, f):
+    log.info("_setEventsKeywords")
     ls = [] 
     for lang, dummy in langs:
         path = '/osha/portal/%s/' % lang
         query = {
                 'portal_type': 'Event',
                 'path': path,
-                'Language': lang,
+                'Language': 'all',
                 }
 
         ls += list(self.portal_catalog(query))
 
-    items, parents = _setKeywords(ls, f)
+    _setKeywords(ls, f)
 
+
+def _centraliseEvents(self, langs, f):
+    log.info("_centraliseEvents")
+    ls = []
     for lang, dummy in langs:
         path = '/osha/portal/%s/' % lang
         query = {
                 'portal_type': 'Event',
                 'path': path,
                 'review_state': 'published',
-                'Language': lang,
+                'Language': 'all',
                 }
 
         for l in self.portal_catalog(query):
@@ -68,30 +84,45 @@ def _centraliseEvents(self, langs, f):
 
             ls.append(l)
 
-    _assignContentRules(self, 'move-events-after-publish', f, langs)
+    items = []
+    for l in ls:
+        try:
+            item = l.getObject() 
+        except:
+            f.write("Couldn't get object! '%s'\n" % l.getPath())
+            log.info("Couldn't get object! '%s'\n" % l.getPath())
+            continue
+
+        items.append(item)
+
     _moveItemsToCentralLocation(self, items, f, 'events')
 
 
-def _centraliseNews(self, langs, f):
+def _setNewsKeywords(self, langs, f):
+    log.info("_setNewsKeywords")
     ls = [] 
     for lang, dummy in langs:
         path = '/osha/portal/%s/' % lang
         query = {
                 'portal_type': 'News Item',
                 'path': path,
-                'Language': lang,
+                'Language': 'all',
                 }
         ls += list(self.portal_catalog(query))
 
-    items, parents = _setKeywords(ls, f)
+    _setKeywords(ls, f)
 
+
+def _centraliseNews(self, langs, f):
+    log.info("_centraliseNews")
+    ls = []
     for lang, dummy in langs:
         path = '/osha/portal/%s/' % lang
         query = {
                 'portal_type': 'News Item',
                 'path': path,
                 'review_state': 'published',
-                'Language': lang,
+                'Language': 'all',
                 }
 
         for l in self.portal_catalog(query):
@@ -113,15 +144,13 @@ def _centraliseNews(self, langs, f):
 
             ls.append(l)
 
-    
-    _assignContentRules(self, 'move-news-after-publish', f, langs)
+    items = [l.getObject() for l in ls]
     _moveItemsToCentralLocation(self, items, f, 'news')
 
 
 def _assignContentRules(self, rule_id, f, langs):
-
+    log.info("_assignContentRules")
     for lang, dummy in langs:
-
         parents = [
             '%s/campaigns/ew2006/' % lang,
             '%s/campaigns/ew2007/' % lang,
@@ -140,41 +169,22 @@ def _assignContentRules(self, rule_id, f, langs):
             try:
                 parent = portal_obj.unrestrictedTraverse(p)
             except:
+                log.info("Couldn't find folder %s for adding content rule %s \n" % (rule_id, parent.absolute_url()))
+                f.write("Couldn't find folder %s for adding content rule %s \n" % (rule_id, parent.absolute_url()))
                 continue
-            # XXX Disabled for dry run...
-            # assignments = IRuleAssignmentManager(parent, None)
-            # get_assignments(storage[rule_id]).insert('/'.join(parent.getPhysicalPath()))
-            # rule_ass = RuleAssignment(ruleid=rule_id, enabled=True, bubbles=True)
-            # assignments[rule_id] = rule_ass
+
+            # XXX DOUBLE CHECK!!!!
+            assignments = IRuleAssignmentManager(parent, None)
+            get_assignments(storage[rule_id]).insert('/'.join(parent.getPhysicalPath()))
+            rule_ass = RuleAssignment(ruleid=rule_id, enabled=True, bubbles=True)
+            assignments[rule_id] = rule_ass
             f.write("Content Rule '%s' assigned to %s \n" % (rule_id, parent.absolute_url()))
             log.info("Content Rule '%s' assigned to %s \n" % (rule_id, parent.absolute_url()))
 
 
 def _setKeywords(ls, f):
-    parents = []
-    items = []
+    log.info("_setKeywords")
     for l in ls:
-        try:
-            item = l.getObject() 
-        except:
-            f.write("Couldn't get object! '%s'\n" % l.getPath())
-            log.info("Couldn't get object! '%s'\n" % l.getPath())
-
-        items.append(item)
-        parent = item.aq_parent
-        subparent = False
-        for p in parents:
-            # we want to make a list of parents to which we assign content rules.
-            # but we don't want to include subfolders because we'll let the content rule bubble.
-            if p.absolute_url() in parent.absolute_url():
-                subparent = True
-                break
-            elif parent.absolute_url() in p.absolute_url():
-                parents.remove(p)
-                 
-        # if parent not in parents and not subparent:
-        parents.append(parent)
-
         for fid, kw  in [ 
                 ('ew2005', 'noise'), 
                 ('ew2006', 'young_people'), 
@@ -184,11 +194,26 @@ def _setKeywords(ls, f):
                 ('riskobservatory', 'risk_observatory'),
                 ]:
             if fid in l.getPath():
-                # item.setCategories(l.getCategories()+[kw])
-                f.write("Add keyword '%s' to %s: %s \n" % (kw, l.portal_type, l.getPath()))
-                log.info("Add keyword '%s' to %s: %s \n" % (kw, l.portal_type, l.getPath()))
+                try:
+                    item = l.getObject() 
+                except:
+                    f.write("Couldn't get object! '%s'\n" % l.getPath())
+                    log.info("Couldn't get object! '%s'\n" % l.getPath())
+                    continue
 
-    return items, parents
+                try:
+                    subject = item.getSubject()
+                except:
+                    subject = item.Schema().getField('subject').get(item)
+
+                if kw not in subject:
+                    subject = list(subject) + [kw]
+                    item.setSubject(subject)
+                    f.write("Add keyword '%s' to %s: %s \n" % (kw, l.portal_type, l.getPath()))
+                    log.info("Add keyword '%s' to %s: %s \n" % (kw, l.portal_type, l.getPath()))
+                else:
+                    f.write("Keyword '%s' already in  %s: %s \n" % (kw, l.portal_type, l.getPath()))
+                    log.info("Keyword '%s' already in %s: %s \n" % (kw, l.portal_type, l.getPath()))
 
 
 def _moveItemsToCentralLocation(self, items, f, folderpath):
@@ -198,11 +223,19 @@ def _moveItemsToCentralLocation(self, items, f, folderpath):
     for item in items:
         fullpath = '%s/%s' % (item.getLanguage() or 'en', folderpath)
         newfolder = portal.unrestrictedTraverse(fullpath)
-        # cookie = item.aq_parent.manage_cutObjects(ids=[item.getId()])
-        # newfolder.manage_pasteOjects(cookie)
+        try:
+            cookie = item.aq_parent.manage_cutObjects(ids=[item.getId()])
+        except ResourceLockedError:
+            item_path = '/'.join(item.getPhysicalPath())
+            f.write("Could not copy '%s' to %s, locked by WEBDAV! \n" % (item_path, fullpath))
+            log.info("Could not copy '%s' to %s, locked by WEBDAV! \n" % (item_path, fullpath))
+            continue
+            
+        newfolder.manage_pasteObjects(cookie)
         item_path = '/'.join(item.getPhysicalPath())
         f.write("'%s' now contains %s \n" % (fullpath, item_path))
         log.info("'%s', now contains %s \n" % (fullpath, item_path))
-        # transaction.savepoint(optimistic=True)
+        transaction.commit()
+
 
 
