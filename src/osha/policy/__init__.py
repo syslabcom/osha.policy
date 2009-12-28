5f839fb81f87d97cbc79161e7c1271518cc7427c
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2009-12-28 17:41:10 +0000

    Remove unused imports

--- src/osha/policy/__init__.py
+++ src/osha/policy/__init__.py
@@ -1,29 +1,18 @@
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from Products.GenericSetup import EXTENSION, profile_registry
+from AccessControl import ModuleSecurityInfo
+
 from Products.CMFCore import DirectoryView
-
 from Products.ATContentTypes.content.document import ATDocument
-import new
 
-from zLOG import LOG, INFO
+import new
+import adapter
+from patches import *
+
 osha_globals= globals()
 
 PROJECTNAME = 'OSHA 3.0'
-def log(msg):
-    LOG(PROJECTNAME, INFO, msg)
-
-import adapter
-
-from patches import *
-
-# Enable getTranslation caching for LinguaPlone
-#from Products.LinguaPlone import config
-#config.CACHE_TRANSLATIONS = 1
-#CACHE_TRANSLATIONS = 1
 
 DirectoryView.registerDirectory('skins', osha_globals)
 
-from AccessControl import ModuleSecurityInfo
 ModuleSecurityInfo('osha.policy.utils').declarePublic('logit')
 
 def initialize(context):
