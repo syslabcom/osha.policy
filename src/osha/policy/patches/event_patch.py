97b04c1fe50de7952ed7d8b9fcdac6544e6c9096
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2010-01-04 14:09:01 +0000

    Remove the schema editing lines, this is now done in a modifier in osha.adaptation

--- src/osha/policy/patches/event_patch.py
+++ src/osha/policy/patches/event_patch.py
@@ -1,17 +1,5 @@
-from Products.ATContentTypes.content.event import ATEvent, ATEventSchema
-from Products.ATContentTypes.content.schemata import finalizeATCTSchema
+from Products.ATContentTypes.content.event import ATEvent
 from types import StringType
-
-# Hide the Event Type field
-ATEventSchema['eventType'].widget.visible = {'edit': 'invisible'}
-ATEventSchema['eventType'].mode = 'r'
-
-# Add the subject field again
-ATEventSchema['subject'].widget.visible = {'edit': 'visible'}
-ATEventSchema['subject'].mode = 'rw'
-
-finalizeATCTSchema(ATEventSchema)
-
 
 def _setEventType(self, value, alreadySet=False, **kw):
     """CMF compatibility method
@@ -24,6 +12,5 @@ def _setEventType(self, value, alreadySet=False, **kw):
         value = ()
     f = self.getField('eventType')
     f.set(self, value, **kw) # set is ok
-
 
 ATEvent.setEventType = _setEventType
