0857185e7ed5ddd12cf8ed244dcdebd08524a85b
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2010-01-20 17:02:38 +0000

    Updated the external method that adds a site manager and registers local schema-extender for the annotatedlinklist field

--- src/osha/policy/Extensions/setLinkListExtension.py
+++ src/osha/policy/Extensions/setLinkListExtension.py
@@ -9,14 +9,13 @@ from Products.CMFCore.utils import getToolByName
 from Products.RALink.content.interfaces import IRALink
 from Products.CaseStudy.interfaces import ICaseStudy
 
-from osha.policy.adapter.schemaextender import LinkListExtender 
+from osha.adaptation.schema import LinkListExtender
 
 def run(self, path=''):
     result = list()
     url_tool = getToolByName(self, 'portal_url')
     portal = url_tool.getPortalObject()
-    obj = None
-
+    obj = self
     if path:
         try:
             obj = portal.restrictedTraverse(path)
@@ -33,18 +32,21 @@ def run(self, path=''):
                             LinkListExtender,
                             (IATEvent,),
                             IOrderableSchemaExtender,
+                            name='event-linklist',
                             )
 
     sitemanager.registerAdapter(
                             LinkListExtender,
                             (IRALink,),
                             IOrderableSchemaExtender,
+                            name='ralink-linklist',
                             )
 
     sitemanager.registerAdapter(
                             LinkListExtender,
                             (ICaseStudy,),
                             IOrderableSchemaExtender,
+                            name='casestudy-linklist',
                             )
 
     result.append("Registered LinkListExtender as adapter on %s" %obj.id)
