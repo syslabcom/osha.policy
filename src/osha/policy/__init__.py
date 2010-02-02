cd38c9fe83592c067a3f142ce98ce9937d1636d8
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-02-02 17:47:21 +0000

    allowing update_object_history() from slc.cleanwordpastedtext for untrusted code, because it's such a lovely and handy method :)

--- src/osha/policy/__init__.py
+++ src/osha/policy/__init__.py
@@ -14,6 +14,7 @@ PROJECTNAME = 'OSHA 3.0'
 DirectoryView.registerDirectory('skins', osha_globals)
 
 ModuleSecurityInfo('osha.policy.utils').declarePublic('logit')
+ModuleSecurityInfo('slc.cleanwordpastedtext.utils').declarePublic('update_object_history')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
