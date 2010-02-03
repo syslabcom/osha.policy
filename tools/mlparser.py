4b345c2fae7b9aa5e6f9f2d2249e9858e344c7ae
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2010-02-03 16:05:03 +0000

    New features

--- tools/mlparser.py
+++ tools/mlparser.py
@@ -1,9 +1,30 @@
 from elementtree.ElementTree import parse, Element, tostring
 from xlrd import open_workbook
+import logging
+import logging.handlers
 
 tree = parse('./src/osha/policy/data/vocabularies/MultilingualThesaurus.vdex')
 
 ns = '{http://www.imsglobal.org/xsd/imsvdex_v1p0}'
+
+osha_handler = logging.FileHandler('osha.log')
+reindex_handler = logging.FileHandler('reindex.log')
+debugging_handler = logging.StreamHandler()
+
+osha_log = logging.getLogger('osha_log')
+reindex_log = logging.getLogger('reindex_log')
+debugging_log = logging.getLogger('debugging_log')
+
+osha_log.addHandler(osha_handler)
+reindex_log.addHandler(reindex_handler)
+debugging_log.addHandler(debugging_handler)
+
+debugging_log.setLevel(logging.WARN)
+osha_log.setLevel(logging.DEBUG)
+reindex_log.setLevel(logging.DEBUG)
+
+deleted_fathers = []
+
 
 items = {}
 
@@ -11,12 +32,24 @@ def get_xls(filename):
     book = open_workbook(file_contents=file(filename).read())
     sheet = book.sheets()[0]
     return sheet
-for term in tree.findall('.//%sterm' % ns):
-    items[term[0].text] = term
+
 fathers = {}
-for term in tree.findall('//%sterm' % ns):
-    for child in term:
-        fathers[child] = term.text
+def updateCaches():
+    for key in fathers.keys():
+        fathers.pop(key)
+    for key in items.keys():
+        items.pop(key)
+    for term in tree.findall('.//%sterm' % ns):
+        debugging_log.debug('terms: \"%s\"', term[0].text)
+        items[term[0].text] = term
+    for term in tree.findall('//%sterm' % ns):
+        for child in term:
+            fathers[child] = term.getchildren()[0].text
+
+updateCaches()
+original_fathers = {}
+for key, value in  fathers.items():
+    original_fathers[key] = value
 
 # All languages
 all_languages = set([x.get('language') for x in tree.findall('.//%slangstring' % ns)])
@@ -46,38 +79,49 @@ new_terms = []
 def register_new_term(line, row):
     new_terms.append((line, row))
 
-def new_term(wrapped):
+def new_term(wrapped, errors):
     line, row = wrapped
     termIdentifier = row[10].value
     termCaption = row[11].value
     termDescription = row[12].value
     assert termIdentifier not in items.keys()
     try:
+        debugging_log.debug(row[17].value.strip())
         parentTerm = items[row[17].value.strip()]
+        debugging_log.debug('parent term found')
     except KeyError:
-        print 'Line %i Can not add new term, provided information is wrong, Father does not exist' % (line)
+        errors.append('Line %i Can not add new term, provided information is wrong, Father "%s" does not exist' % (line, row[17].value.strip()))
         return True
     new_term =  new_tree_term(termIdentifier, termCaption, termDescription)
     last_brother = None
+    has_brothers = False
     for index, brother in enumerate(parentTerm):
         if brother.tag != ns + 'term':
             continue
+        else:
+            has_brothers = True
         brotherTermIdentifier = brother[0].text
         if termIdentifier < brotherTermIdentifier:
             parentTerm.insert(index, new_term)
+            last_brother = None
             break
+        last_brother = index
+    if last_brother:
+        parentTerm.insert(last_brother, new_term)
+    if not has_brothers:
+        parentTerm.insert(index, new_term)
     return False
 
 amended_terms = []
 def register_amend_term(line, row):
     amended_terms.append((line, row))
 
-def amend_term(wrapped):
+def amend_term(wrapped, errors):
     line, row = wrapped
     try:
         term = items[row[0].value]
     except KeyError:
-        print "Line: %i Can not amend term, if term id is missing" % (line)
+        errors.append("Line: %i Can not amend term, if term id is missing" % line)
         return True
     new_term = new_tree_term('ignore', row[11].value, 'ignore')
     caption = new_term[1]
@@ -87,9 +131,19 @@ def amend_term(wrapped):
 
 moved_terms = {}
 def register_move_term(line, row):
-    old = row[13].value
-    new = row[14].value
-    father = row[17].value
+    old = row[13].value.strip()
+    new = row[14].value.strip()
+    if '+' in old:
+        for one_old in old.split('+'):
+            one_old = one_old.strip()
+            if inner_register_move_term(line, row, one_old, new):
+                return True
+        return False
+    else:
+        return inner_register_move_term(line, row, old, new)
+
+def inner_register_move_term(line, row, old, new):
+    father = row[17].value.strip()
     if not moved_terms.has_key(old):
         moved_terms[old] = {}
     data = moved_terms[old]
@@ -98,53 +152,101 @@ def register_move_term(line, row):
     data['lines'].append(line)
     if not data.has_key('new'):
         data['new'] = new
-    assert data['new'] == new, "For the key %s two entries exist that do not agree, where to add the new key to: 1: %s, 2:%s" % (old, data['new'], new)
-    if not data.get('father', '') and father:
-        data['father'] = father
+    if data['new'] != new:
+        data['error'] = "Lines: %s For the key %s two entries exist that do not agree, where to add the new key to: 1: %s, 2:%s" % (data['lines'], old, data['new'], new)
+    type = row[9].value.strip()
     if father:
-        assert data.get('father', '') == father, str((data['father'], '=', father))
-def move_term(old, data):
+        if not data.get('father', ''):
+            data['father'] = father
+            if type == 'New term':
+                data['father_authorative'] = True
+            else:
+                data['father_authorative'] = False
+        else:
+            if data.get('father', '') != father:
+                problem = 'Lines: %s identifiers %s of broader term do not match for the involved lines' % (str(data['lines']), ', '.join(['"%s"' % x for x in [data['father'], father]]))
+                if type == 'New term':
+                    if not data['father_authorative']:
+                        data['father_authorative'] = True
+                        data['father'] = father
+                        debugging_log.warning(problem)
+                    else:
+                        data['error'] = problem
+                else:
+                    debugging_log.warning(problem)
+
+def move_term(old, data, errors):
+    if len(data['lines']) == 1:
+        errors.append('Lines: %s I was asked to perform a move operation, but I have only one excel line related to it.' % str(data['lines']))
+        return True
+    if data.has_key('error'):
+        errors.append('Lines: %s Cannot move term, %s' % (data['lines'], data['error']))
+        return True
     try:
         old_child = items[old]
-    except:
-        print "Lines: %s Cannot move term, because old id does not exist" % (data['lines'])
+    except KeyError:
+        errors.append("Lines: %s Cannot move term, because old id does not exist" % data['lines'])
         return True
     try:
-        old_father = items[fathers[old_child]]
-    except:
-        print "Lines: %s This cannot be moved, father of %s unknown" % (str(data['lines']), items[old][0].text)
+        old_father_id = original_fathers[old_child]
+        if old_father_id not in deleted_fathers:
+            old_father = items[original_fathers[old_child]]
+            deleted = False
+        else:
+            deleted = True
+    except KeyError:
+        errors.append("Lines: %s This cannot be moved, father of %s unknown" %(str(data['lines']), items[old][0].text))
         return True
-    old_father.remove(old_child)
     new = data['new']
-    father = items[data['father']]
+    try:
+        data['father']
+    except KeyError:
+        errors.append('Lines: %s missing new parent, where shall I add the category?' % str(data['lines']))
+        return True
+    try:
+        parentTerm = items[data['father']]
+    except KeyError:
+        errors.append("Lines: %s parent \"%s\" not found, don't know where to add" % (str(data['lines']), data['father']))
+        return True
     old_child[0].text = new
+    last_brother = None
+    has_brothers = False
     for index, brother in enumerate(parentTerm):
         if brother.tag != ns + 'term':
             continue
+        else:
+            has_brothers = True
         brotherTermIdentifier = brother[0].text
         if new < brotherTermIdentifier:
             parentTerm.insert(index, old_child)
+            last_brother = None
             break
+        last_brother = index
+    if last_brother:
+        parentTerm.insert(last_brother, old_child)
+    if not has_brothers:
+        parentTerm.insert(index, old_child)
+    deleted_fathers.append(old)
+    if not deleted:
+        old_father.remove(old_child)
+    reindex_log.info('move old-new: %s-%s' % (old, new))
     return False
-   
 
 deleted_terms = []
 def register_delete_term(line, row):
     deleted_terms.append((line, row))
 
-def delete_term(wrapped):
+def delete_term(wrapped, errors):
     i, row = wrapped
-    try:
-        father = items[row[17].value]
-        kid = items[row[0].value]
-        father.remove(kid)
-    except KeyError:
-        print 'Line %i Can not delete, because information is incomplete' % (i)
-        return True
+    kid_id = row[0].value
+    kid = items[kid_id]
+    father_id = fathers[kid]
+    father = items[father_id]
+    father.remove(kid)
+    reindex_log.info('Delete key: %s' % kid_id)
     return False
-        
 
-sheet = get_xls('./new_thesaurus.xls')
+sheet = get_xls('./new_thesaurus2.xls')
 for i in range(sheet.nrows):
     row = sheet.row(i)
     if not (row[9].value or row[13].value):
@@ -159,18 +261,47 @@ for i in range(sheet.nrows):
         try:
             register_move_term(i+1, row)
         except AssertionError, e:
-            print e
+            debugging_log.exception("Something went wrong")
     elif row[9].value == u'Accept / Reject':
         continue
     else:
-        print row
+        debugging_log.error("Unknown task: %s", row)
         break
 
-filter(delete_term, deleted_terms)
-filter(new_term, new_terms)
-filter(amend_term, amended_terms)
-for key, value in moved_terms.items():
-    move_term(key, value)
+old_tasks = -1
+tasks = 0
+while old_tasks != tasks:
+    errors = []
+    old_tasks = tasks
+    debugging_log.info('Tasks: %i', tasks)
+    new_new_terms = []
+    for term in new_terms:
+        if new_term(term, errors):
+            new_new_terms.append(term)
+    new_terms = new_new_terms
+    new_amended_terms = []
+    for term in amended_terms:
+        if amend_term(term, errors):
+            new_amended_terms.append(term)
+    amended_terms = new_amended_terms
+    new_moved_terms = {}
+    for key, value in moved_terms.items():
+        if move_term(key, value, errors):
+            new_moved_terms[key] = value
+    moved_terms = new_moved_terms
+    tasks = len(deleted_terms) + len(new_terms) + len(amended_terms) + len(moved_terms)
+    updateCaches()
 
+new_deleted_terms = []
+for term in deleted_terms:
+    if delete_term(term, errors):
+        new_deleted_terms.append(term)
+deleted_terms = new_deleted_terms
+
+errors = list(set(errors))
+errors.sort()
+for error in errors:
+    osha_log.warning(error)
+    
 tree.write('out.xml')
     
