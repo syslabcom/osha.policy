051658c231f108db78dedf04a98d1318e56da250
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2010-03-22 10:54:20 +0000

    Translation tool and the result added

---
+++ tools/translations.py
@@ -0,0 +1,43 @@
+from elementtree.ElementTree import parse
+from itertools import ifilter
+import sys
+import csv
+
+tree = parse(sys.argv[1])
+ns = '{http://www.imsglobal.org/xsd/imsvdex_v1p0}'
+
+all_terms = [term for term in tree.findall('//%sterm' % ns)]
+parents = {}
+for term in all_terms:
+    for children in term[3:]:
+        parents[children[0].text] = term
+
+def getParents(term):
+    id = term[0].text
+    try:
+        parent = parents[id]
+    except KeyError:
+        return []
+    retval = getParents(parent)
+    retval.append(getEnglishTranslation(parent))
+    return retval
+
+def getEnglishTranslation(term):
+    for subterm in term.findall('%scaption/%slangstring' % (ns, ns)):
+        if subterm.attrib['language'] == 'en':
+            return subterm.text
+
+def filterRule(a):
+    return len(set([x.text for x in a.findall('%scaption/%slangstring' % (ns, ns))])) < 2
+
+untranslated_terms = ifilter(filterRule, all_terms)
+
+translations = ['id', 'Path (for context only)', 'Term to translate','translation']
+
+writer = csv.writer(open('translate.csv', 'wb'))
+writer.writerow(translations)
+for term in untranslated_terms:
+    id = term[0].text
+    ttt = term[1][0].text
+    path = ' -> '.join(getParents(term))
+    writer.writerow([id, ttt, path, ''])
