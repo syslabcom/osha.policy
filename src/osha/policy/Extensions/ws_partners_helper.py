005b91ab69fd5819bfa51640e609f7d5821ec6b8
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-07-05 10:11:14 +0000

    make python values out of "True", "False" and "None"

--- src/osha/policy/Extensions/ws_partners_helper.py
+++ src/osha/policy/Extensions/ws_partners_helper.py
@@ -2,6 +2,12 @@ import re
 from logging import getLogger
 log = getLogger('osha.policy::ws_partners_helper')
 
+REPLACE = {
+    "True": True,
+    "False": False,
+    "None": None
+    }
+
 def parsePartners(self, txt, size):
     partners = dict()
     if type(txt) == type(''):
@@ -15,7 +21,8 @@ def parsePartners(self, txt, size):
                 log.error('Got a line of size %d, but it should be %d' % (len(li),
                     size))
             else:
-                partners[li[0]] = li
+                vals = [REPLACE.get(x, x) for x in li]
+                partners[li[0]] = vals
         except:
             log.error("Could not eval line: %s" % line)
     return partners
