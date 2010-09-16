2182efec1d848f38a90bf80479a9beb07c706f17
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-09-16 15:26:08 +0000

    since there's a new event handler in slc.publications for updating chapters in a translation, wen need to overwrite it here too

--- src/osha/policy/adapter/publication_customisations.py
+++ src/osha/policy/adapter/publication_customisations.py
@@ -1,6 +1,14 @@
-from slc.publications.adapter.events import ChapterUpdater
+from slc.publications.adapter.events import ChapterUpdater, TranslationChapterUpdater
 
 class OshaChapterUpdater(ChapterUpdater):
+    """
+    The Standard Chapter Updater does a workflow setting that we don't
+    want. Our Chapter Links don't have a workflow at all
+    """
+    def setState(self, chapter, language):
+        pass
+
+class OshaTranslationChapterUpdater(TranslationChapterUpdater):
     """
     The Standard Chapter Updater does a workflow setting that we don't
     want. Our Chapter Links don't have a workflow at all
