09cc48501d58e6651ba5152a20fda862065db312
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2010-11-15 14:13:26 +0000

    Issue #1861: Refactored the Calendar's event provider to not show events from the /oshnetwork folder on the main (or other non-oshnetwork) event folders.

--- src/osha/policy/adapter/calendar_eventprovider.py
+++ src/osha/policy/adapter/calendar_eventprovider.py
@@ -1,13 +1,16 @@
 from zope import interface
 from zope import component
-from p4a.calendar import interfaces
-from Products.CMFCore import utils as cmfutils
-from Products.Archetypes import atapi
-from Products.ATContentTypes.content import topic
-from Products.CMFCore.utils import getToolByName
 from zope.component import getMultiAdapter
 
-from p4a.plonecalendar.eventprovider import _make_zcatalog_query, ATEventProvider as BASEATEventProvider, TopicEventProvider as BASETopicEventProvider
+from Products.AdvancedQuery import In, And, Not, Eq, Ge, Le
+from Products.CMFCore import utils as cmfutils
+from Products.Archetypes import atapi
+from Products.CMFCore.utils import getToolByName
+
+from p4a.calendar import interfaces
+from p4a.plonecalendar.eventprovider import _make_zcatalog_query
+from p4a.plonecalendar.eventprovider import \
+        ATEventProvider as BASEATEventProvider
 
 class ATEventProvider(BASEATEventProvider):
     interface.implements(interfaces.IEventProvider)
@@ -16,54 +19,66 @@ class ATEventProvider(BASEATEventProvider):
     def __init__(self, context):
         self.context = context
 
-    def gather_events(self, start=None, stop=None, **kw):
-        
+    def gather_events(self, 
+                start=None, 
+                stop=None, **kw):
         catalog = cmfutils.getToolByName(self.context, 'portal_catalog')
+
+        # search in the navigation root of the currently selected language 
+        # and in the canonical root path with Language = preferredLanguage or 
+        # neutral
+        pstate = getMultiAdapter(
+                        (self.context, self.context.request), 
+                        name=u'plone_portal_state'
+                        )
+
+        nav_root_path = pstate.navigation_root_path()
+        paths = [nav_root_path]
+
+        nav_root = pstate.portal().restrictedTraverse(nav_root_path)
+        try:
+            canonical_path = '/'.join(nav_root.getCanonical().getPhysicalPath())
+        except AttributeError:
+            pass
+        else:
+            if canonical_path not in paths:
+                paths.append(canonical_path)
+
         portal_languages = getToolByName(self.context, 'portal_languages')
         preflang = portal_languages.getPreferredLanguage()
 
-        # search in the navigation root of the currently selected language and in the canonical path
-        # with Language = preferredLanguage or neutral
-        paths = list()
-        portal_state = getMultiAdapter((self.context, self.context.request), name=u'plone_portal_state')
-        navigation_root_path = portal_state.navigation_root_path()
-        paths.append(navigation_root_path)
-        try:
-            navigation_root = portal_state.portal().restrictedTraverse(navigation_root_path)
-            canonical_path = '/'.join(navigation_root.getCanonical().getPhysicalPath())
-            paths.append(canonical_path)
-        except:
-            pass
 
-        kw.update(dict(path=paths, Language=['', preflang]))
+        query = And(
+            Eq('portal_type', 'Event'), 
+            In('path', paths), 
+            In('Language', ['', preflang]),
+            )
+
+        # We don't want to show events from Focal Points, in the oshnetwork/
+        # folder, in Calenders outside of these FOPs.
+        oshaview = getMultiAdapter(
+                    (self.context, self.context.request), 
+                    name=u'oshaview'
+                    )
+        subsite = oshaview.getCurrentSubsite()
+        if subsite is None:
+            query = And(query, Not(In('path', [p + '/oshnetwork' for p in paths])))
+
+        # Not sure where this key comes from, but it is not an index...
+        if '-C' in kw:
+            del kw['-C']
+
         kw = _make_zcatalog_query(start, stop, kw)
-        event_brains = catalog(portal_type='Event', **kw)
+        for key, value in kw.items():
+            if key in ['start', 'end']:
+                if value['range'] == 'max': 
+                    query = And(query, Le(key, value['query']))
+                else:
+                    query = And(query, Ge(key, value['query']))
+            else:
+                query = And(query, Eq(key, value)) 
+
+        event_brains = catalog.evalAdvancedQuery(query, (('Date', 'desc'),))
         return (interfaces.IEvent(x) for x in event_brains)
 
 
-    # Not needed at the moment
-#class TopicEventProvider(BASETopicEventProvider):
-#    interface.implements(interfaces.IEventProvider)
-#    component.adapts(topic.ATTopic)
-#
-#    def __init__(self, context):
-#        self.context = context
-#        
-#    def gather_events(self, start=None, stop=None, **kw):
-#        print "custom gather_events of TopicEventProvider"
-#        kw = _make_zcatalog_query(start, stop, kw)
-#
-#        # This sad hack allows us to overwrite whatever restriction
-#        # the topic makes to the date.  Providing the 'start' and
-#        # 'date' arguments to the 'queryCatalog' method would
-#        # otherwise just overwrite our own date criteria.
-#        # See http://plone4artists.org/products/plone4artistscalendar/issues/35
-#        catalog = cmfutils.getToolByName(self.context, 'portal_catalog')
-#        def my_catalog(request, **kwargs):
-#            kwargs.update(kw)
-#            return catalog(request, **kwargs)
-#        self.context.portal_catalog = my_catalog
-#        self.context.portal_catalog.searchResults = my_catalog
-#        value = (interfaces.IEvent(x) for x in self.context.queryCatalog())
-#        del self.context.portal_catalog
-#        return value
