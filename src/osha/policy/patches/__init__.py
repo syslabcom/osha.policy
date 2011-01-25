10ce120bd1c4e9a7b080c701a05797db45bd2e4e
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-01-25 13:56:45 +0000

    Preparing release 1.3.30

--- src/osha/policy/patches/__init__.py
+++ src/osha/policy/patches/__init__.py
@@ -7,7 +7,6 @@ import newsitem_patch
 import I18NBaseObject_preserve_shortname
 import email_as_login_patch
 import event_patch
-import gocept_linkchecker
 import ploneflashuploadpatch
 import ploneportlets_getGroup_allow_admin
 import linguaplone_defaultLanguage_patch
