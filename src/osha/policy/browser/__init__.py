48130f945a48d254fd11f78fbe5b4854e7be67df
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-04-05 12:04:49 +0000

    added BrowserView getHostname, so that we can display the hostname in error pages

--- src/osha/policy/browser/__init__.py
+++ src/osha/policy/browser/__init__.py
@@ -1,2 +1,10 @@
 
 # browser package
+from Products.Five import BrowserView
+import socket
+
+class HostnameView(BrowserView):
+    """Utility view"""
+
+    def __call__(self, **args):
+        return socket.gethostname()
