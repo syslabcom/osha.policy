6d4139c694dccb7d80335b5bad4b2920f8d66fcc
brand <brand@97d050b9-b799-4550-a901-4f04598d4601>
2011-05-12 20:28:46 +0000

    Remove the "try except ImportError" for Products.LinguaPlone. It is a dependency in osha.policy and these 'try excepts' can cause potential circular imports and other problems.
    See do3cc's blog: http://do3cc.blogspot.com/2010/08/dont-catch-import-errors-use.html

--- src/osha/policy/browser/PressroomHelper.py
+++ src/osha/policy/browser/PressroomHelper.py
@@ -1,16 +1,9 @@
-from interfaces import IPressroomHelper
-from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
-from Products.CMFPlone import utils
+
+from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
-try:
-    from Products.LinguaPlone.interfaces import ITranslatable
-except ImportError:
-    HAS_LINGUAPLONE = False
-else:
-    HAS_LINGUAPLONE = True
 
-
+from interfaces import IPressroomHelper
 
 class PressroomHelper(BrowserView):
     implements(IPressroomHelper)
@@ -20,12 +13,12 @@ class PressroomHelper(BrowserView):
         textpieces = list()
         field = self.context.getField('referenced_content')
         objs = field.getAccessor(self.context)()
-        # If LinguaPlone is present, look for translated versions, but fall back
-        #  to original if no translation is available
-        if HAS_LINGUAPLONE:
-            language_tool = getToolByName(self.context, 'portal_languages')
-            lang = language_tool.getPreferredLanguage()
-            objs = [obj.getTranslation(lang) or obj for obj in objs]
+        # Look for translated versions, but fall back
+        # to original if no translation is available 
+        # (requires Products.LinguaPlone)
+        language_tool = getToolByName(self.context, 'portal_languages')
+        lang = language_tool.getPreferredLanguage()
+        objs = [obj.getTranslation(lang) or obj for obj in objs]
         for obj in objs:
             textpieces.append(obj.getText())
 
