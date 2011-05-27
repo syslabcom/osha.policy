e886b2c6b47989c4d8c00d16a151a6711ad74f77
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-05-27 15:31:30 +0000

    napofilm data-structure (just begun...)

---
+++ src/osha/policy/data/multimedia/napofilm.py
@@ -0,0 +1,32 @@
+filmstructure = [
+    { "id"          : "napo-015-safe-moves",
+      "title"       : "heading_napo-015-safe-moves",
+      "description" : "description_napo-015-safe-moves",
+      "episodes"    : [
+            { "id"         :
+                  "napo-015-safe-moves-episode-001-planning-for-safety",
+              },
+            { "id"         :
+                  "napo-015-safe-moves-episode-002-danger-unloading",
+              },
+            { "id"         :
+                  "napo-015-safe-moves-episode-003-poor-maintenance",
+              },
+            { "id"         :
+                  "napo-015-safe-moves-episode-004-carried-away",
+              }
+            ]
+    },
+    dict(id="napo-001-best-signs-story",
+     title="title_napo_film_1",
+     description="description_napo_film_1",
+     episodes= [
+        dict(id="napo-001-best-signs-story-episode-001-start",
+            title="title_napo_episode_1_0"),
+        dict(id="napo-001-best-signs-story-episode-010-hearing-protection-must-be-worn",
+            title="title_napo_episode_1_9"),
+        dict(id="napo-001-best-signs-story-episode-016-fire-extinguishing-equipment",
+            title="title_napo_episode_1_15"),
+     ] 
+    )
+]
