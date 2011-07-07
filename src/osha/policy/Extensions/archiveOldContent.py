44f086aabe747a941fb32dbe2beaeb889af36a6c
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-07-07 14:21:24 +0000

    added helper script for archiving content by type and date

---
+++ src/osha/policy/Extensions/archiveOldContent.py
@@ -0,0 +1,59 @@
+import logging
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
+from slc.linguatools.utils import toggle_outdated
+
+allowed_types = ['News Item', 'Event', 'Press Release']
+
+logger = logging.getLogger('osha.policy.archiveOldContent')
+
+
+def archiveByType(self, portal_type=None, age_in_days=None, limit=0):
+    if not portal_type:
+        return "You need to specify portal_type"
+    if portal_type not in allowed_types:
+        return "Only the following portal types are allowed: %s" %\
+            ', '.join(allowed_types)
+    if not age_in_days:
+        return "You must provide age_in_days to indicate up to which day " \
+            "content should become outdated. Example: age_in_days=180 for " \
+            "6 months and older."
+    try:
+        days = int(age_in_days)
+    except:
+        return "The value passed for age_in_days is not a valid integer"
+    try:
+        limit = int(limit)
+    except:
+        return "The value passed for limit is not a valid integer"
+    pc = getToolByName(self, 'portal_catalog')
+    now = DateTime()
+    date = now - days
+    # hardcoded to corporate site
+    query = dict(portal_type=portal_type,
+        path="/osha/portal/en",
+        Language='all',
+        modified=dict(query=date, range='max'))
+    res = pc(query)
+    logger.info("I have %d results" % len(res))
+    messages = list()
+    if limit > 0:
+        res = res[:limit]
+    for r in res:
+        try:
+            obj = r.getObject()
+        except:
+            logger.warn("Error getting object for %s" % r.getPath())
+            continue
+        for lang in obj.getTranslations():
+            trans = obj.getTranslation(lang)
+            logger.info('Handling item %s' % trans.absolute_url())
+            view = trans.restrictedTraverse('@@object_toggle_outdated')
+            msg = view.toggle(True)
+            messages.append(msg)
+
+    ret = "<html>%d objects were modified. The following actions were "\
+        "performed:<ul><li>%s</ul></html>" %  \
+        (len(messages), "</li><li>".join(messages))
+    return ret
