5657d8b262fb87dce2e1aa6465b623cdc3d7ec4f
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-07-07 15:53:15 +0000

    better logging, intermediate commits

--- src/osha/policy/Extensions/archiveOldContent.py
+++ src/osha/policy/Extensions/archiveOldContent.py
@@ -1,6 +1,8 @@
 import logging
+import transaction
 from DateTime import DateTime
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import getSiteEncoding
 from Products.statusmessages.interfaces import IStatusMessage
 from slc.linguatools.utils import toggle_outdated
 
@@ -8,8 +10,48 @@ allowed_types = ['News Item', 'Event', 'Press Release']
 
 logger = logging.getLogger('osha.policy.archiveOldContent')
 
+br = '<br />'
+link = '<a href="%(url)s">%(text)s</a>' + br
+
+class LogWriter(object):
+
+    def __init__(self, response=None):
+        self.response = response
+
+    def write(self, msg):
+        if self.response:
+            if isinstance(msg, unicode):
+                msg = msg.encode(getSiteEncoding(self))
+            self.response.write(msg + br)
+        logger.info(msg)
+
+    def close(self):
+        self.log.close()
+
+
+def setupLog(self, response):
+    log = LogWriter(response)
+    if response:
+        response.setHeader('Content-Type', 'text/html;charset=UTF-8')
+        response.setHeader('Expires', 'Sat, 1 Jan 2000 00:00:00 GMT')
+        response.setHeader('Pragma', 'no-cache')
+        self.backlink = link % dict(url=self.absolute_url() + '/manage_workspace',
+                               text='Back to ZMI')
+        response.write('<html><body>')
+        response.write(self.backlink)
+        response.write(br)
+    return log
+
+def finish(self, response):
+    if response:
+        response.write(br)
+        response.write(self.backlink)
+
+
 
 def archiveByType(self, portal_type=None, age_in_days=None, limit=0):
+    response = self.REQUEST and self.REQUEST.RESPONSE or None
+    log = setupLog(self, response)
     if not portal_type:
         return "You need to specify portal_type"
     if portal_type not in allowed_types:
@@ -27,6 +69,10 @@ def archiveByType(self, portal_type=None, age_in_days=None, limit=0):
         limit = int(limit)
     except:
         return "The value passed for limit is not a valid integer"
+    log.write('<h2>Bulk-outdate content</h2>')
+    log.write('Starting at ' + `DateTime().ISO()`)
+    log.write('<h3>Settings</h3><p>portal_type: %s, age_in_days: %d, limit: %d</p>' % (
+        portal_type, days, limit))
     pc = getToolByName(self, 'portal_catalog')
     now = DateTime()
     date = now - days
@@ -37,7 +83,8 @@ def archiveByType(self, portal_type=None, age_in_days=None, limit=0):
         modified=dict(query=date, range='max'))
     res = pc(query)
     logger.info("I have %d results" % len(res))
-    messages = list()
+    trans_count = 0
+    count = 0
     if limit > 0:
         res = res[:limit]
     for r in res:
@@ -53,9 +100,14 @@ def archiveByType(self, portal_type=None, age_in_days=None, limit=0):
             view = trans.restrictedTraverse('@@object_toggle_outdated')
             msg = view.toggle(True)
             msg += u" (<a href='%(url)s'>%(url)s</a>)" % dict(url=url)
-            messages.append(msg)
+            log.write(msg)
+            trans_count += 1
+        count += 1
+        if count % 10 == 0:
+            transaction.commit()
+            log.write("<br /><strong>Committing after %s items</strong><br />" % count)
 
-    ret = "<html>%d objects were modified. The following actions were "\
-        "performed:<ul><li>%s</ul></html>" %  \
-        (len(messages), u"</li><li>".join(messages))
-    return ret
+    log.write('<br/><br/>Finished at ' + `DateTime().ISO()`)
+    log.write('A total of %d objects (including translations) were modified.' % trans_count)
+    finish(self, response)
+
