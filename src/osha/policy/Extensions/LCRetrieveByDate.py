e689d1a6ea762a246d48053733917e7c91e20e2e
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-08-26 09:03:10 +0000

    removed "if 1"

--- src/osha/policy/Extensions/LCRetrieveByDate.py
+++ src/osha/policy/Extensions/LCRetrieveByDate.py
@@ -28,43 +28,42 @@ def LCRetrieveByDate(self, skiplist=[]):
 #    self.portal_catalog.reindexIndex('portal_linkchecker_uid', self.REQUEST)
     try:
         # gather all objects that are of a type we can check for links
-        if 1:
-            objects = self.portal_catalog(Language='all', modified={'query':sincedate,'range':'min'})
-            os_ = len(objects)
-            zLOG.LOG('CMFLinkChecker', zLOG.INFO, "%d objects will be crawled" % os_)
-            i = 0
-            for ob in objects:
-                i += 1
-                zLOG.LOG("CMFLinkChecker", zLOG.BLATHER,
-                         "Site Crawl Status",
-                         "%s of %s (%s)" % (i, os_, ob.getPath()))
-                ob = ob.getObject()
-                if ob is None:
-                    # Maybe the catalog isn't up to date
-                    continue
-                outdated = IAnnotatable.providedBy(ob) and \
-                    IAnnotations(ob).get(ANNOTATION_KEY, False) or False
-                if outdated:
-                    zLOG.LOG("CMFLinkChecker", zLOG.BLATHER, "unregistering, object is outdated")
-                    lc.database.unregisterObject(ob)
-                    continue
-                try:
-                    state = pwt.getInfoFor(ob, 'review_state')
-                except:
-                    state = "undefined"
-                if state not in ALLOWED_STATES:
-                    zLOG.LOG("CMFLinkChecker", zLOG.BLATHER, "unregistering, object is not public: %s" % state)
-                    lc.database.unregisterObject(ob)
-                    continue
-                #try:
-                lc.retrieving.retrieveObject(ob, online=False)
-                #except Exception,e:
-                #    zLOG.LOG('CMFLinkChecker', zLOG.BLATHER,
-                #      "Unable to retrieveObject for %s. Error: %s" %([ob], e))
-                if not i % 500 :
-                    transaction.savepoint()
-                    zLOG.LOG('CMFLinkChecker', zLOG.INFO,
-                        "Crawling site - commited after %d objects" %(i))
+        objects = self.portal_catalog(Language='all', modified={'query':sincedate,'range':'min'})
+        os_ = len(objects)
+        zLOG.LOG('CMFLinkChecker', zLOG.INFO, "%d objects will be crawled" % os_)
+        i = 0
+        for ob in objects:
+            i += 1
+            zLOG.LOG("CMFLinkChecker", zLOG.BLATHER,
+                     "Site Crawl Status",
+                     "%s of %s (%s)" % (i, os_, ob.getPath()))
+            ob = ob.getObject()
+            if ob is None:
+                # Maybe the catalog isn't up to date
+                continue
+            outdated = IAnnotatable.providedBy(ob) and \
+                IAnnotations(ob).get(ANNOTATION_KEY, False) or False
+            if outdated:
+                zLOG.LOG("CMFLinkChecker", zLOG.BLATHER, "unregistering, object is outdated")
+                lc.database.unregisterObject(ob)
+                continue
+            try:
+                state = pwt.getInfoFor(ob, 'review_state')
+            except:
+                state = "undefined"
+            if state not in ALLOWED_STATES:
+                zLOG.LOG("CMFLinkChecker", zLOG.BLATHER, "unregistering, object is not public: %s" % state)
+                lc.database.unregisterObject(ob)
+                continue
+            #try:
+            lc.retrieving.retrieveObject(ob, online=False)
+            #except Exception,e:
+            #    zLOG.LOG('CMFLinkChecker', zLOG.BLATHER,
+            #      "Unable to retrieveObject for %s. Error: %s" %([ob], e))
+            if not i % 500 :
+                transaction.savepoint()
+                zLOG.LOG('CMFLinkChecker', zLOG.INFO,
+                    "Crawling site - commited after %d objects" %(i))
     finally:
         pass
         #server.setClientNotifications(True)
