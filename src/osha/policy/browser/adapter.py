e2c37d6811828c9be05de19155f81583fa72203a
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-08-30 09:47:26 +0000

    preparation for handling outdated toggle events with regards to link-checker

--- src/osha/policy/browser/adapter.py
+++ src/osha/policy/browser/adapter.py
@@ -9,3 +9,11 @@ class SEPFolder(object):
     """
     implements(ISEPFolder)
     adapts(ISEPFolder)
+
+
+def handleToggleOutdated(event):
+    """If an object was marked as outdated, remove it from the linkchecker
+    (unregister). If the outdated flag was removed, register it again. """
+    pass
+    # To be done once we have plone.app.async in place
+
