9cc4d712ba43bd9c7bf42c98e0d7a0f44afe3ece
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2011-09-21 06:26:31 +0000

    Preparing release 1.3.39

--- src/osha/policy/patches/pressroom_patch.py
+++ src/osha/policy/patches/pressroom_patch.py
@@ -1,9 +1,13 @@
 from Products.PressRoom.content.PressRelease import schema as PressReleaseSchema
 from Products.PressRoom.content.PressRelease import PressRelease
 
-PressReleaseSchema['subhead'].languageIndependent = False
-PressReleaseSchema['releaseDate'].languageIndependent = True
-PressReleaseSchema['releaseContacts'].languageIndependent = True
+###
+# Moved to osha.adaptation.schema.PressReleaseModifier
+###
+
+# PressReleaseSchema['subhead'].languageIndependent = False
+# PressReleaseSchema['releaseDate'].languageIndependent = True
+# PressReleaseSchema['releaseContacts'].languageIndependent = True
 
 def Date(self):
     """Avoid the need for a getStoryDate metadatum in the catalog"""
