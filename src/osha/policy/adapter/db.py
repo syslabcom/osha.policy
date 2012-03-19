a0c3dd830b81c435075828a57dcf3b299c1889e8
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-03-19 15:06:39 +0000

    Depends on collective.lead and is getting called by some setuphandler

--- src/osha/policy/adapter/db.py
+++ src/osha/policy/adapter/db.py
@@ -1,7 +1,7 @@
 from persistent import Persistent 
 from zope.interface import implements 
 from zope.component import getUtility 
-from collective.lead import Database
+#from collective.lead import Database
 from osha.policy.interfaces import IDatabaseSettings 
 from sqlalchemy.engine.url import URL 
 from sqlalchemy import Table
@@ -18,22 +18,22 @@ class OSHADatabaseSettings(Persistent):
     password = None 
     database = '' 
 
-class OSHADatabase(Database): 
+# class OSHADatabase(Database): 
     
-    @property 
-    def _url(self): 
-        settings = getUtility(IDatabaseSettings) 
-        return URL(drivername=settings.drivername,  
-                    username=settings.username, 
-                    password=settings.password, host=settings.hostname, 
-                    port=settings.port, database=settings.database) 
+#     @property 
+#     def _url(self): 
+#         settings = getUtility(IDatabaseSettings) 
+#         return URL(drivername=settings.drivername,  
+#                     username=settings.username, 
+#                     password=settings.password, host=settings.hostname, 
+#                     port=settings.port, database=settings.database) 
                     
-    def _setup_tables(self, metadata, tables): 
-        pass
-        #tables['screening'] = Table('screening',  
-        #metadata, autoload=True) 
-        #tables['reservation'] = Table('reservation',  
-        #metadata, autoload=True) 
+#     def _setup_tables(self, metadata, tables): 
+#         pass
+#         #tables['screening'] = Table('screening',  
+#         #metadata, autoload=True) 
+#         #tables['reservation'] = Table('reservation',  
+#         #metadata, autoload=True) 
 
-    def _setup_mappers(self, tables, mappers): 
-        pass
+#     def _setup_mappers(self, tables, mappers): 
+#         pass
