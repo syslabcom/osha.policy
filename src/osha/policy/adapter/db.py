1dd0059391992e45084251006852062f1ac9cc10
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-04-10 09:24:34 +0000

    pep8

--- src/osha/policy/adapter/db.py
+++ src/osha/policy/adapter/db.py
@@ -1,39 +1,40 @@
-from persistent import Persistent 
-from zope.interface import implements 
-from zope.component import getUtility 
+from persistent import Persistent
+from zope.interface import implements
+from zope.component import getUtility
 from collective.lead import Database
-from osha.policy.interfaces import IDatabaseSettings 
-from sqlalchemy.engine.url import URL 
+from osha.policy.interfaces import IDatabaseSettings
+from sqlalchemy.engine.url import URL
 from sqlalchemy import Table
-from sqlalchemy.orm import mapper, relation 
+from sqlalchemy.orm import mapper, relation
 
-    
-class OSHADatabaseSettings(Persistent): 
 
-    implements(IDatabaseSettings) 
-    drivername = 'postgres' 
-    hostname = 'localhost' 
-    port = None 
-    username = '' 
-    password = None 
-    database = '' 
+class OSHADatabaseSettings(Persistent):
 
-class OSHADatabase(Database): 
-    
-    @property 
-    def _url(self): 
-        settings = getUtility(IDatabaseSettings) 
-        return URL(drivername=settings.drivername,  
-                    username=settings.username, 
-                    password=settings.password, host=settings.hostname, 
-                    port=settings.port, database=settings.database) 
-                    
-    def _setup_tables(self, metadata, tables): 
+    implements(IDatabaseSettings)
+    drivername = 'postgres'
+    hostname = 'localhost'
+    port = None
+    username = ''
+    password = None
+    database = ''
+
+
+class OSHADatabase(Database):
+
+    @property
+    def _url(self):
+        settings = getUtility(IDatabaseSettings)
+        return URL(drivername=settings.drivername,
+                    username=settings.username,
+                    password=settings.password, host=settings.hostname,
+                    port=settings.port, database=settings.database)
+
+    def _setup_tables(self, metadata, tables):
         pass
-        #tables['screening'] = Table('screening',  
-        #metadata, autoload=True) 
-        #tables['reservation'] = Table('reservation',  
-        #metadata, autoload=True) 
+        #tables['screening'] = Table('screening',
+        #metadata, autoload=True)
+        #tables['reservation'] = Table('reservation',
+        #metadata, autoload=True)
 
-    def _setup_mappers(self, tables, mappers): 
+    def _setup_mappers(self, tables, mappers):
         pass
