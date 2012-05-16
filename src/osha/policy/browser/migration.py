25e63fe7b1cb2ecad15f7c84a6462acb7ff0cfb3
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-05-16 14:42:22 +0000

    various improvements, we now do real intermedia commits

--- src/osha/policy/browser/migration.py
+++ src/osha/policy/browser/migration.py
@@ -16,6 +16,10 @@ from Products.CMFCore.utils import getToolByName
 from Products.contentmigration.basemigrator.walker import Walker
 from plone.app.blob.migrations import ATImageToBlobImageMigrator
 
+import logging
+
+
+LOG = logging.getLogger('P4 image2blob migration')
 
 count = 0
 
@@ -29,8 +33,8 @@ class ZopeFindImageWalker(Walker):
         self.dst_portal_type = 'Image'
         self.dst_meta_type = 'ATBlob'
 
-        self.transaction_size = int(kwargs.get('transaction_size', 20))
-        self.full_transaction = kwargs.get('full_transaction', False)
+        self.transaction_size = int(kwargs.get('transaction_size', 50))
+        self.full_transaction = kwargs.get('full_transaction', True)
         self.use_savepoint = kwargs.get('use_savepoint', False)
 
         if self.full_transaction and self.use_savepoint:
@@ -49,41 +53,61 @@ class ZopeFindImageWalker(Walker):
         self.migrate(self.walk(), **kwargs)
 
     def walk(self):
-        top_node = self.portal.unrestrictedTraverse(
+        node = self.portal.unrestrictedTraverse(
             "/osha/portal")
-
-        for item in self._walk(top_node):
-            if item is not None:
-                yield itemh
-
-    def _walk(self, node):
-        global count
-        if node.meta_type in ["Script (Python)", "External Method"]:
-            return None
-        for id, ob in node.ZopeFind(node, search_sub=0):
-            if ob.meta_type == "ATImage":
-                count += 1
-                if count % 100 == 0:
-                    print "Committing %s" %count
-                    transaction.commit()
-                print "%s %s" %(count, ob.absolute_url(1))
-                if ob.getImage() is "":
-                    print "Deleting %s" %ob.absolute_url(1)
-                    parent = ob.aq_explicit.aq_parent
-                    parent.manage_delObjects(ob.getId())
-                else:
-                    return ob
-            else:
-                # print "%s" %ob.absolute_url(1)
-                self._walk(ob)
-        return []
+        # while/blacklist for individual image ids
+	id_whitelist = []
+        id_blacklist = []
+        # top-level images:
+        for id, ob in node.ZopeFind(
+            node, obj_metatypes=["ATImage",],search_sub=0):
+            if len(id_whitelist) and id not in id_whitelist:
+                continue
+            if id in id_blacklist:
+                continue
+            if ob.getImage() == '':
+                LOG.info('Image %s is empty, skipping' % ob.absolute_url(1))
+                continue
+            LOG.info("Migrating %s" %ob.absolute_url(1))
+            yield ob
+        folders = node.objectValues(['ATBTreeFolder', 'ATFolder'])
+        # top-level folders
+        # white/blacklist for folder ids
+        whitelist = []
+	blacklist = ['']
+	for folder in folders:
+            if len(whitelist) and folder.id not in whitelist:
+                LOG.info("Whitelist. Skipping top-level folder %s" % folder.id)
+                continue
+            if len(blacklist) and folder.id in blacklist:
+                LOG.info("Blacklist. Skipping top-level folder %s" % folder.id)
+                continue
+            for id, ob in folder.ZopeFind(
+                folder, obj_metatypes=["ATImage",],search_sub=1):
+		if len(id_whitelist) and id not in id_whitelist:
+		    continue
+                if id in id_blacklist:
+                    continue
+	        if ob.getImage() == '':
+		    LOG.info('Image %s is empty, skipping' % ob.absolute_url(1))
+                    continue
+                LOG.info("Migrating %s" %ob.absolute_url(1))
+                yield ob
 
 
 class MigrateImagesView(BrowserView):
+    """ NOTE: Apply "plone.app.blob: 'Image' replacement type" Types
+    tool in portal_setup before runnig this.
+    """
 
     def __call__(self):
         portal = getSite()
-        import pdb; pdb.set_trace()
+
+        # Getting a Doomed transaction for the first commit with the
+        # sample database. Doing one transaction.commit() here is a
+        # workaround
+        # import pdb; pdb.set_trace()
+        transaction.abort()
 
         start_time = datetime.now()
 
@@ -92,5 +116,5 @@ class MigrateImagesView(BrowserView):
         walker.go()
         transaction.commit()
 
-        print "Finished after %s seconds" %(datetime.now() - start_time)
+        LOG.info("Finished after %s seconds" %(datetime.now() - start_time))
 
