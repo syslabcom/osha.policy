c7b75ef38d7947ae94b114a4f925fb92d3b9ccc2
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2012-07-05 16:23:30 +0000

    Add an external method to set default_output_type = "text/html" on content which has it set to "text/x-html-safe". This caused getText to transform the raw body to content which included extra escaped html, refs #5378

---
+++ src/osha/policy/Extensions/set_text_field_output_type.py
@@ -0,0 +1,40 @@
+from logging import getLogger
+
+import transaction
+from zope.app.component.hooks import getSite
+
+logger = getLogger('osha.policy/set_text_field_output_type')
+
+
+def main(self):
+    portal = getSite()
+    pc = portal.portal_catalog
+
+    query = {
+        "portal_type":[
+            'RichDocument', 'Blog Entry', 'Document', 'RALink',
+            'PressContact', 'PressRoom', 'HelpCenterFAQ', 'Event',
+            'Topic', 'CallForContractors', 'PublicJobVacancy',
+            'PressRelease', 'News Item', 'OSH_Link', 'SPSpeech']}
+
+    results = pc.searchResults(query)
+    broken_brains = []
+    for count, brain in enumerate(results):
+        try:
+            obj = brain.getObject()
+            text_field = obj.Schema().getField("text")
+        except Exception, e:
+            broken_brains.append("%s: %s" %(brain.getPath(), e))
+            continue
+        if (text_field
+            and hasattr(text_field, "default_output_type")
+            and text_field.default_output_type == "text/x-html-safe"):
+            text_field.default_output_type = "text/html"
+            logger.info(
+                "Set default_output_type on %s %s" %(
+                    obj.portal_type, obj.absolute_url(1)))
+        if count % 1000 == 0:
+            transaction.commit()
+    logger.info("Possibly invalid brains: %s" % broken_brains)
+    transaction.commit()
+    return "Done"
