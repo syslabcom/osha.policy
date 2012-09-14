a42507488ea4ede54d3d7070bfbbb64d5c4eeb04
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-09-14 14:48:05 +0000

    we have a new version of the addTrtanslation Patch for linguaplone. Reason: Translated items should keep their short names, not build them from the translated title #5793

--- src/osha/policy/patches/__init__.py
+++ src/osha/policy/patches/__init__.py
@@ -7,7 +7,7 @@ import collective_solr_patch
 #import email_as_login_patch
 import event_patch
 import increase_resourceregistry_cache_duration
-#import linguaplone_addTranslation_patch
+import linguaplone_addTranslation_patch
 #import linguaplone_defaultLanguage_patch
 import linguaplone_config_patch
 import newsitem_patch
