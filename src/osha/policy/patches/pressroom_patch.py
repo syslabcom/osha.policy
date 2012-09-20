677718dc9685d1bfcc7ad86e3b115564f1c04ccb
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-09-20 15:11:06 +0000

    we can't make fields languageIndependent via patch any more apparently #5791

--- src/osha/policy/patches/pressroom_patch.py
+++ src/osha/policy/patches/pressroom_patch.py
@@ -2,8 +2,8 @@ from Products.PressRoom.content.PressRelease import schema as PressReleaseSchema
 from Products.PressRoom.content.PressRelease import PressRelease
 
 PressReleaseSchema['subhead'].languageIndependent = False
-PressReleaseSchema['releaseDate'].languageIndependent = True
-PressReleaseSchema['releaseContacts'].languageIndependent = True
+#PressReleaseSchema['releaseDate'].languageIndependent = True
+#PressReleaseSchema['releaseContacts'].languageIndependent = True
 
 def Date(self):
     """Avoid the need for a getStoryDate metadatum in the catalog"""
