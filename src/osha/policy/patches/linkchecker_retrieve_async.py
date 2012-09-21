73d82dbbbc1b310432fcf59d29fb467f0df5aa40
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-09-21 08:39:50 +0000

    patch the index() method of gocept.linkchecker.link.Link to be able to run it smoothly in async

--- src/osha/policy/patches/linkchecker_retrieve_async.py
+++ src/osha/policy/patches/linkchecker_retrieve_async.py
@@ -1,6 +1,35 @@
-from gocept.linkchecker import retrievemanager 
+from gocept.linkchecker import retrievemanager
+from gocept.linkchecker.link import Link
+from gocept.linkchecker.url import URL
 from osha.policy import handlers
 
 retrievemanager.update_links = handlers.update_links
 retrievemanager.remove_links = handlers.remove_links
 
+
+def index(self):
+    if self.getObject() is None:
+        self.getParentNode().manage_delObjects([self.id])
+        return
+    try:
+        url = self.getURL()
+    except ValueError:
+        # Make sure the URL object exists when we index ourselves.
+        url = URL(self.url())
+        db = self.getLinkCheckerDatabase()
+        if url.id not in db.objectIds():
+            db._setObject(url.id, url)
+        url = db[url.id]
+    except AttributeError, err:
+        # if the retriever is called a second time in async, the URL object
+        # will aready exist. A getObject() on the brain in getURL will
+        # fail, due to missing REQUEST
+        # We therefore ignore this kind of error and assume the first run was
+        # already successful
+        if err.message == 'REQUEST':
+            return
+    path = '/'.join(self.getPhysicalPath())
+    self.link_catalog.catalog_object(self, path)
+
+
+Link.index = index
