2ced312a906ef53b7469954566e76afa93e082a6
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2012-10-05 16:58:18 +0000

    don't kill translation references when language is set to neutral

--- src/osha/policy/patches/linguaplone_addTranslation_patch.py
+++ src/osha/policy/patches/linguaplone_addTranslation_patch.py
@@ -1,3 +1,5 @@
+from Acquisition import aq_parent, aq_inner
+from Products.Archetypes.utils import shasattr
 from Products.LinguaPlone.I18NBaseObject import I18NBaseObject
 from Products.LinguaPlone.I18NBaseObject import AlreadyTranslated
 from Products.LinguaPlone import config
@@ -12,6 +14,9 @@ from zope.event import notify
 # We call unmarkCreationFlag on all translations, so that the id (short-name)
 # of translations is NOT built from the title
 # Osha ticket #5793
+
+# Second reason
+# We don't want to lose all translation references when an item is set to language neutral
 
 
 def addTranslation(self, language, *args, **kwargs):
@@ -31,7 +36,10 @@ def addTranslation(self, language, *args, **kwargs):
     factory = ITranslationFactory(self)
     translation = factory.createTranslation(
         parent, language, *args, **kwargs)
-    translation.unmarkCreationFlag()
+    # special handling for Belgium
+    ppath = '/'.join(parent.getPhysicalPath())
+    if not ppath.startswith('/osha/portal/fop/belgium'):
+        translation.unmarkCreationFlag()
     translation.reindexObject()
     notify(events.ObjectTranslatedEvent(self, translation, language))
 
@@ -39,3 +47,51 @@ def addTranslation(self, language, *args, **kwargs):
 
 
 I18NBaseObject.addTranslation = addTranslation
+
+
+def setLanguage(self, value, **kwargs):
+    """Sets the language code.
+
+    When changing the language in a translated folder structure,
+    we try to move the content to the existing language tree.
+    """
+    # If we are called during a schema update we should not be
+    # deleting any language relations or complaining about already
+    # existing translations.  A schema update saves the current
+    # value, sets the default language (at which point there can
+    # easily be two English translations if that is the default
+    # language) and restores the original value again.  So really
+    # there is no reason for doing anything other than setting the
+    # value.
+    req = getattr(self, 'REQUEST', None)
+    if shasattr(req, 'get'):
+        if req.get('SCHEMA_UPDATE', None) is not None:
+            # We at least should set the field.
+            self.getField('language').set(self, value, **kwargs)
+            return
+
+    translation = self.getTranslation(value)
+    if self.hasTranslation(value):
+        if translation == self:
+            return
+        else:
+            raise AlreadyTranslated(translation.absolute_url())
+    self.getField('language').set(self, value, **kwargs)
+
+    # THIS IS THE PATCH
+    # we don't kill all references when an item is set to neutral
+####     if not value:
+####        self.deleteReferences(RELATIONSHIP)
+
+    parent = aq_parent(aq_inner(self))
+
+    locator = ILocateTranslation(self)
+    new_parent = locator.findLocationForTranslation(value)
+
+    if new_parent != parent:
+        info = parent.manage_cutObjects([self.getId()])
+        new_parent.manage_pasteObjects(info)
+    self.reindexObject()
+    self._catalogRefs(self)
+
+I18NBaseObject.setLanguage = setLanguage
