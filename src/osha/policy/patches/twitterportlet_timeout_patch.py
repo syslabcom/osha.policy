039126f8bd361c99ca12869b6eafaa8992cb3fe7
wthomas <wthomas@97d050b9-b799-4550-a901-4f04598d4601>
2013-02-08 11:08:28 +0000

    EXPERIMENTAL (=untested) patch for the Twitter portlet code to introduce a timeout

---
+++ src/osha/policy/patches/twitterportlet_timeout_patch.py
@@ -0,0 +1,44 @@
+from fourdigits.portlet.twitter.fourdigitsportlettwitter import Renderer
+import socket
+
+
+REDUCED_TIMEOUT = 5
+
+def _getuserinfo(self):
+    """Get twitter user info"""
+    orignal_timeout = socket.getdefaulttimeout()
+    socket.setdefaulttimeout(REDUCED_TIMEOUT)
+    userinfo = self.twapi.GetUser(self.data.username)
+    socket.setdefaulttimeout(orignal_timeout)
+    return userinfo
+
+def gettweetsofuser(self, username, userpictures, includerts):
+    """Return the tweets of a certain user"""
+    orignal_timeout = socket.getdefaulttimeout()
+    try:
+        socket.setdefaulttimeout(REDUCED_TIMEOUT)
+        tweets = self.twapi.GetUserTimeline(username,
+                                            include_rts=includerts,
+                                            include_entities=True)
+    except:
+        tweets = []
+    finally:
+         socket.setdefaulttimeout(orignal_timeout)
+    return tweets
+
+def gettweetsbysearch(self, query_dict):
+    """Return tweets based on a search query"""
+    orignal_timeout = socket.getdefaulttimeout()
+    try:
+        socket.setdefaulttimeout(REDUCED_TIMEOUT)
+        tweets = self.twapi.GetSearch(**query_dict)
+    except:
+        tweets = []
+    finally:
+         socket.setdefaulttimeout(orignal_timeout)
+    return tweets
+
+
+Renderer._getuserinfo = _getuserinfo
+Renderer.gettweetsofuser = gettweetsofuser
+Renderer.gettweetsbysearch = gettweetsbysearch
