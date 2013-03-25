e196e3598ace290892dabe997a5efaa0c8e29f40
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2013-03-25 15:04:04 +0000

    #7038 register a link checker retriever for SPSpeaker items

---
+++ src/osha/policy/adapter/lmsretrievers.py
@@ -0,0 +1,28 @@
+from slc.seminarportal.content.speaker import SPSpeaker
+
+import gocept.linkchecker.interfaces
+import gocept.linkchecker.utils
+import zope.component
+import zope.interface
+
+
+class SPSpeakerRetriever(object):
+    """Retriever for documents with one or more RichText widgets.
+
+    SPSpeaker doesn't implement Products.Archetypes.atapi.BaseContent
+    so the default ATGeneral retriever isn't registered for it."""
+
+    zope.component.adapts(SPSpeaker)
+    zope.interface.implements(gocept.linkchecker.interfaces.IRetriever)
+
+    def __init__(self, context):
+        self.context = context
+
+    def retrieveLinks(self):
+        """Finds all links from the object and return them."""
+        return gocept.linkchecker.utils.retrieveAllRichTextFields(self.context)
+
+    def updateLink(self, oldurl, newurl):
+        """Replace all occurances of <oldurl> on object with <newurl>."""
+        gocept.linkchecker.utils.updateAllRichTextFields(
+            oldurl, newurl, self.context)
