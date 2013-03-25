6e724a872c2e9d77eba1959eaa61a1b58cefb040
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2013-03-25 15:52:03 +0000

    #7038 register a link checker retriever for Topic items

--- src/osha/policy/adapter/lmsretrievers.py
+++ src/osha/policy/adapter/lmsretrievers.py
@@ -1,5 +1,5 @@
 from slc.seminarportal.content.speaker import SPSpeaker
-
+from Products.ATContentTypes.interfaces.topic import IATTopic
 import gocept.linkchecker.interfaces
 import gocept.linkchecker.utils
 import zope.component
@@ -13,6 +13,28 @@ class SPSpeakerRetriever(object):
     so the default ATGeneral retriever isn't registered for it."""
 
     zope.component.adapts(SPSpeaker)
+    zope.interface.implements(gocept.linkchecker.interfaces.IRetriever)
+
+    def __init__(self, context):
+        self.context = context
+
+    def retrieveLinks(self):
+        """Finds all links from the object and return them."""
+        return gocept.linkchecker.utils.retrieveAllRichTextFields(self.context)
+
+    def updateLink(self, oldurl, newurl):
+        """Replace all occurances of <oldurl> on object with <newurl>."""
+        gocept.linkchecker.utils.updateAllRichTextFields(
+            oldurl, newurl, self.context)
+
+
+class TopicRetriever(object):
+    """Retriever for documents with one or more RichText widgets.
+
+    ATTopic doesn't implement Products.Archetypes.atapi.BaseContent
+    so the default ATGeneral retriever isn't registered for it."""
+
+    zope.component.adapts(IATTopic)
     zope.interface.implements(gocept.linkchecker.interfaces.IRetriever)
 
     def __init__(self, context):
