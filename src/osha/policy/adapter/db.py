becdb7c8d89401684aeec58c2e124329c09e5c65
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2013-05-24 15:17:01 +0000

    Revert db adapter to implement IDatabaseSetting

--- src/osha/policy/adapter/db.py
+++ src/osha/policy/adapter/db.py
@@ -1,28 +1,18 @@
-from collective.lead import Database
-from App.config import getConfiguration
+from persistent import Persistent
+from zope.interface import implements
+from zope.component import getUtility
+from osha.policy.interfaces import IDatabaseSettings
+from sqlalchemy.engine.url import URL
+from sqlalchemy import Table
+from sqlalchemy.orm import mapper, relation
 
 
-class OSHADatabase(Database):
+class OSHADatabaseSettings(Persistent):
 
-    @property
-    def _url(self):
-        """Return db connection string which we read from the environment."""
-        configuration = getConfiguration()
-        try:
-            conf = configuration.product_config['osha.policy']
-            url = conf['osha.database']
-        except (AttributeError, KeyError):
-            raise KeyError(
-                'No product config found! Cannot read osha.database '
-                'connection string.'
-            )
-        return url
-
-    def _setup_tables(self, metadata, tables):
-        """XXX: We need to implement this method, because
-        collective.lead expects it, but we don't realy need it, since
-        we're working directly with the database.  """
-        pass
-
-    def _setup_mappers(self, tables, mappers):
-        pass
+    implements(IDatabaseSettings)
+    drivername = 'postgres'
+    hostname = 'localhost'
+    port = None
+    username = ''
+    password = None
+    database = ''
