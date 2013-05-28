adfc12560546ed77bc720fbc707da1a47357a08d
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2013-05-28 14:37:41 +0000

    #7648 Re-enable postgres db adapter for lc_maintenance

--- src/osha/policy/adapter/db.py
+++ src/osha/policy/adapter/db.py
@@ -1,10 +1,8 @@
+from App.config import getConfiguration
+from collective.lead import Database
+from osha.policy.interfaces import IDatabaseSettings
 from persistent import Persistent
 from zope.interface import implements
-from zope.component import getUtility
-from osha.policy.interfaces import IDatabaseSettings
-from sqlalchemy.engine.url import URL
-from sqlalchemy import Table
-from sqlalchemy.orm import mapper, relation
 
 
 class OSHADatabaseSettings(Persistent):
@@ -16,3 +14,29 @@ class OSHADatabaseSettings(Persistent):
     username = ''
     password = None
     database = ''
+
+
+class OSHADatabase(Database):
+
+    @property
+    def _url(self):
+        """Return db connection string which we read from the environment."""
+        configuration = getConfiguration()
+        try:
+            conf = configuration.product_config['osha.policy']
+            url = conf['osha.database']
+        except (AttributeError, KeyError):
+            raise KeyError(
+                'No product config found! Cannot read osha.database '
+                'connection string.'
+            )
+        return url
+
+    def _setup_tables(self, metadata, tables):
+        """XXX: We need to implement this method, because
+        collective.lead expects it, but we don't realy need it, since
+        we're working directly with the database.  """
+        pass
+
+    def _setup_mappers(self, tables, mappers):
+        pass
