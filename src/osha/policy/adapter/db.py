6e34979e50891e85df967851caf6ee9aa786037c
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2013-06-04 13:18:48 +0000

    Cleanup: remove adapter db config, it's not used and makes it awkward to run tests
    The lc_maintenance views don't use the collective.lead database
    adapter any more, it uses sqlalchemy directly

--- src/osha/policy/adapter/db.py
+++ src/osha/policy/adapter/db.py
@@ -1,9 +1,10 @@
-from App.config import getConfiguration
-from collective.lead import Database
+from logging import getLogger
 from osha.policy.interfaces import IDatabaseSettings
 from persistent import Persistent
 from zope.interface import implements
 
+
+log = getLogger("osha.policy")
 
 class OSHADatabaseSettings(Persistent):
 
@@ -14,29 +15,3 @@ class OSHADatabaseSettings(Persistent):
     username = ''
     password = None
     database = ''
-
-
-class OSHADatabase(Database):
-
-    @property
-    def _url(self):
-        """Return db connection string which we read from the environment."""
-        configuration = getConfiguration()
-        try:
-            conf = configuration.product_config['osha.policy']
-            url = conf['osha.database']
-        except (AttributeError, KeyError):
-            raise KeyError(
-                'No product config found! Cannot read osha.database '
-                'connection string.'
-            )
-        return url
-
-    def _setup_tables(self, metadata, tables):
-        """XXX: We need to implement this method, because
-        collective.lead expects it, but we don't realy need it, since
-        we're working directly with the database.  """
-        pass
-
-    def _setup_mappers(self, tables, mappers):
-        pass
