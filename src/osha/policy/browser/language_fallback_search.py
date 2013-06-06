2175c72684b8a67c953179fd376aaed9fb6da459
reinhard <reinhard@97d050b9-b799-4550-a901-4f04598d4601>
2013-06-06 17:01:20 +0000

    added language fallback search that uses the solr search utility, refs #7529

--- src/osha/policy/browser/language_fallback_search.py
+++ src/osha/policy/browser/language_fallback_search.py
@@ -1,7 +1,13 @@
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
+from collective.solr.flare import PloneFlare
+from collective.solr.interfaces import ISearch
 from plone import api
+from zope.component import queryUtility
 
+import logging
+
+log = logging.getLogger(__name__)
 
 class LanguageFallbackSearch(BrowserView):
     """OSHA requires that searches return translations in the
@@ -35,3 +41,37 @@ class LanguageFallbackSearch(BrowserView):
         # Return all results except originals, leaving preferred
         # language translations and untranslated documents:
         return [x for x in search_results if x.UID not in original_uids]
+        
+    def search_solr(self, query, **parameters):
+        """
+        """
+        
+        lang_tool = api.portal.get_tool("portal_languages")
+        search = queryUtility(ISearch)
+        if search is None:
+            log.warn('Could not get solr ISearch utility')
+            return []
+        rc = api.portal.get_tool("reference_catalog")
+
+        # Search for both canonical, language-neutral and preferred language
+        # translations.
+        preferred_lang = lang_tool.getPreferredLanguage()
+        languages = ["en", "all"]
+        if preferred_lang not in languages:
+            languages.append(preferred_lang)
+        query = ' '.join((query,
+                          "+Language:({0})".format(' OR '.join(languages))))
+        search_results = search(
+            query,
+            **parameters)
+
+        # Find the originals of the preferred language translations:
+        translation_uids = [x.UID for x in search_results
+                            if x.Language not in ['en', '']]
+        originals = rc.search({"relationship": "translationOf",
+                               "sourceUID": translation_uids})
+        original_uids = [x.targetUID for x in originals]
+
+        # Return all results except originals, leaving preferred language translations
+        # and untranslated documents:
+        return [PloneFlare(x) for x in search_results if x.UID not in original_uids]
--- src/osha/policy/tests/test_language_fallback_search.py
+++ src/osha/policy/tests/test_language_fallback_search.py
@@ -1,4 +1,7 @@
 from Products.CMFPlone.utils import getToolByName
+from collective.solr.interfaces import ISearch, ISolrConnectionConfig
+from collective.solr.search import Search
+from collective.solr.manager import SolrConnectionManager, SolrConnectionConfig
 from osha.policy.tests.base import OSHA_INTEGRATION_TESTING
 from plone import api
 from plone.app.testing import TEST_USER_ID
@@ -8,6 +11,7 @@ from plone.app.testing import setRoles
 from zope.component import getMultiAdapter
 
 import unittest2 as unittest
+from mock import MagicMock
 
 
 class TestLanguageFallbackSearch(unittest.TestCase):
@@ -36,6 +40,21 @@ class TestLanguageFallbackSearch(unittest.TestCase):
         self.assertEqual(set([x.getPath() for x in results]),
                          set(['/plone/events', '/plone/events/aggregator', '/plone/events/en-event-de']))
 
+    @unittest.skip("some problem with mock_search")
+    def test_fallback_search_solr(self):
+        """Basic test to test if the sendto method works (with basic
+        template and no extra keyword arguments).  """
+        mock_results = ['/plone/events', '/plone/events/aggregator', '/plone/events/en-event-de', '/plone/events/en-event']
+        mock_search = Search()
+        mock_search.getManager = lambda: SolrConnectionManager(active=True)
+        mock_search.__call__ = MagicMock(return_value=mock_results)
+        mock_search.search = mock_search.__call__
+        sm = self.portal.getSiteManager()
+        sm.registerUtility(component=SolrConnectionConfig(), provided=ISolrConnectionConfig)
+        sm.registerUtility(component=mock_search, provided=ISearch)
+        lf_search_view = self.portal.restrictedTraverse("@@language-fallback-search")
+        results = lf_search_view.search_solr("path_parents:/plone/events")
+        mock_search.assert_called_with("path_parents:/plone/events +Language:en OR all OR de")
 
 def test_suite():
     """This sets up a test suite that actually runs the tests in
