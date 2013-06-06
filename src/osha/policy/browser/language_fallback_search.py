72f972b910de05c706a60e7bc4fabd795b858929
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2013-06-06 14:02:19 +0000

    Tidy up: wrap long lines

--- src/osha/policy/browser/language_fallback_search.py
+++ src/osha/policy/browser/language_fallback_search.py
@@ -11,12 +11,13 @@ class LanguageFallbackSearch(BrowserView):
     def search(self, query):
         """
         """
-        
+
         lang_tool = api.portal.get_tool("portal_languages")
         pc = api.portal.get_tool("portal_catalog")
         rc = api.portal.get_tool("reference_catalog")
 
-        # Search for both canonical, langauge-neutral and preferred language translations.
+        # Search for both canonical, langauge-neutral and preferred
+        # language translations.
         preferred_lang = lang_tool.getPreferredLanguage()
         languages = ["en", ""]
         if preferred_lang not in languages:
@@ -25,12 +26,12 @@ class LanguageFallbackSearch(BrowserView):
         search_results = pc.search(query)
 
         # Find the originals of the preferred language translations:
-        translation_uids = [x.UID for x in search_results if x.Language not in ['en', '']]
-        originals = rc.search({"relationship":"translationOf", "sourceUID": translation_uids})
+        translation_uids = [
+            x.UID for x in search_results if x.Language not in ['en', '']]
+        originals = rc.search(
+            {"relationship":"translationOf", "sourceUID": translation_uids})
         original_uids = [x.targetUID for x in originals]
 
-        # Return all results except originals, leaving preferred language translations
-        # and untranslated documents:
+        # Return all results except originals, leaving preferred
+        # language translations and untranslated documents:
         return [x for x in search_results if x.UID not in original_uids]
-        
-        
